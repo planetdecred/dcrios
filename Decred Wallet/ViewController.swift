89c847fb5de1f60f2cd6f495615db313b69cbe1c
Suleiman Abubakar <abubakars29@yahoo.com>
2018-02-06 01:32:02 +0100

    Initial Commit

---
+++ Decred Wallet/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Decred Wallet
+//
+//  Created by Suleiman Abubakar on 06/02/2018.
+//  Copyright © 2018 Macsleven. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
