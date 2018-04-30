0268bde157f70b86c86a6de600453fac75331c87
Suleiman Abubakar <abubakars29@yahoo.com>
2018-04-30 17:55:37 +0100

    updated copyrigh information

--- Decred Wallet/View Controller/encryptWalletViewController.swift
+++ Decred Wallet/View Controller/encryptWalletViewController.swift
@@ -1,10 +1,8 @@
 //
 //  encryptWalletViewController.swift
 //  Decred Wallet
-//
-//  Created by Suleiman Abubakar on 05/04/2018.
-//  Copyright © 2018 Macsleven. All rights reserved.
-//
+//  Copyright © 2018 The Decred developers.
+//  see LICENSE for details.
 
 import Foundation
 import UIKit
