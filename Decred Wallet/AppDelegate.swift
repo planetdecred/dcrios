4bd3f1328d88536f78e0feae52f18d045d1c70cc
Suleiman Abubakar <abubakars29@yahoo.com>
2018-05-20 15:42:56 +0100

    fixed start up flow

--- Decred Wallet/AppDelegate.swift
+++ Decred Wallet/AppDelegate.swift
@@ -61,11 +61,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-       /* showAnimatedStartScreen()
+        showAnimatedStartScreen()
         AppContext.instance.storage = Storage()
         AppContext.instance.walletManager = MobilewalletNewLibWallet(NSHomeDirectory()+"/Documents")
-        AppContext.instance.walletManager?.initLoader()*/
-        createMenuView()
+        AppContext.instance.walletManager?.initLoader()
         return true
     }
     
