2f8548a84ce9a923e8f36506d0c3a20d392fc0af
Philipp Maluta <fil.the.ensoreus@gmail.com>
2018-05-24 15:04:23 +0300

    Dcr connection support

--- Decred Wallet/AppDelegate.swift
+++ Decred Wallet/AppDelegate.swift
@@ -62,9 +62,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         showAnimatedStartScreen()
-        AppContext.instance.storage = Storage()
-        AppContext.instance.walletManager = WalletNewLibWallet(NSHomeDirectory()+"/Documents")
-        AppContext.instance.walletManager?.initLoader()
+                UserDefaults.standard.setValuesForKeys(["pref_user_name" : "N8BdMGtxvM1+QmEiGe609mn1JHA=",
+                                                "pref_user_passwd":"Afgz/vjdPLQAv5Crsz9J94dIT1g=",
+                                                "pref_server_ip": "192.168.0.104:19109"])
+        AppContext.instance.decrdConnection = DcrdConnection()
+        AppContext.instance.decrdConnection?.initiateWallet()
         return true
     }
     
