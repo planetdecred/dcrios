13148bbfe4674ad3b41c75afd38ec9f9e3889358
Rohit <rktr09@gmail.com>
2018-05-24 20:49:23 +0530

    Added transaction history screen and Table cell

---
+++ Decred Wallet/View Controller/TransactionHistoryViewController.swift
@@ -0,0 +1,78 @@
+//
+//  TransactionHistoryViewController.swift
+//  Decred Wallet
+//
+//  Created by rails on 23/05/18.
+//  Copyright © 2018 The Decred developers. All rights reserved.
+//
+
+import UIKit
+
+class TransactionHistoryViewController: UIViewController {
+    
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var btnFilter: DropMenuButton!
+    
+    var dic1 = [ "status":"Pending", "type" : "Credit", "amount" : "112.000000 DCR","date" : "23 Mar, 2018 10:30 pm" ] as Dictionary!
+    var dic2 = [ "status":"Confirmed", "type" : "Debit", "amount" : "24.000000 DCR","date" : "23 Mar, 2018 10:30 pm" ] as Dictionary!
+    var dic3 = [ "status":"Confirmed", "type" : "Debit", "amount" : "26.000000 DCR","date" : "23 Mar, 2018 10:30 pm" ] as Dictionary!
+    var dic4 = [ "status":"Confirmed", "type" : "Debit", "amount" : "72.000000 DCR","date" : "23 Mar, 2018 10:30 pm" ] as Dictionary!
+    var dic5 = [ "status":"Confirmed", "type" : "Credit", "amount" : "92.000000 DCR","date" : "23 Mar, 2018 10:30 pm" ] as Dictionary!
+    
+    let filterMenu = ["ALL", "Regular", "Ticket", "Votes", "Revokes", "Sent"] as [String]
+    
+    var mainContens = [Dictionary<String, String>]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+         mainContens.append(dic1!)
+         mainContens.append(dic2!)
+         mainContens.append(dic3!)
+         mainContens.append(dic4!)
+         mainContens.append(dic5!)
+        
+         btnFilter.initMenu(filterMenu) {(index, value) in
+            print("index : \(index), Value : \(value)")
+         }
+        
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+
+extension TransactionHistoryViewController : UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.mainContens.count
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 60.0
+    }
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+     
+        tableView.register(UINib(nibName: TransactionHistoryTableViewCell.identifier, bundle: nil), forCellReuseIdentifier: TransactionHistoryTableViewCell.identifier)
+        let cell = self.tableView.dequeueReusableCell(withIdentifier: "TransactionHistoryTableViewCell") as! TransactionHistoryTableViewCell
+        
+        let data = TransactionTableViewCellData(data: mainContens[indexPath.row])
+        cell.setData(data)
+        return cell
+    }
+}
