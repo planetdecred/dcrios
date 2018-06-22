d2c499c86b11a82c79174541a6df845319e523c4
Philipp Maluta <fil.the.ensoreus@gmail.com>
2018-06-22 20:13:58 +0300

    DcrSendTransactionProtocol basic implementation

--- Decred Wallet/DcrdConnection/DcrTransactionTraffic.swift
+++ Decred Wallet/DcrdConnection/DcrTransactionTraffic.swift
@@ -11,19 +11,19 @@ import Wallet
 
 protocol DcrSendTransactionProtocol: DcrdBaseProtocol{
     func prepareTransaction(from account:Int32, to address:String, amount:Int64, shouldBeConfirmed:Bool)
-    func signTransaction(transaction: WalletConstructTxResponse) -> Data?
-    func publish(transaction:Data)
+    func signTransaction(transaction: WalletConstructTxResponse, password:Data) -> Data?
+    func publish(transaction:Data)  -> Data?
 }
 
 extension DcrSendTransactionProtocol{
-    func prepareTransaction(from account:Int32, to address:String, amount:Int64, shouldBeConfirmed:Bool){
-        wallet?.constructTransaction(address, amount: amount, srcAccount: account, requiredConfirmations: shouldBeConfirmed)
+    func prepareTransaction(from account:Int32, to address:String, amount:Int64, shouldBeConfirmed:Bool) throws {
+        try wallet?.constructTransaction(address, amount: amount, srcAccount: account, requiredConfirmations:Int32(NSNumber(booleanLiteral: shouldBeConfirmed)))
     }
-    func signTransaction(transaction: WalletConstructTxResponse) -> Data?{
-        return nil
+    func signTransaction(transaction: WalletConstructTxResponse, password:Data) throws -> Data?  {
+        return try wallet?.signTransaction(transaction.unsignedTransaction(), privPass: password)
     }
-    func publish(transaction:Data){
-        
+    func publish(transaction:Data) throws -> Data?{
+        return try wallet?.publishTransaction(transaction)
     }
 }
 
