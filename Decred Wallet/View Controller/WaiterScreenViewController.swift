97aaad17b1b829d8aa930127a279d9782f2dcb43
Rohit <rktr09@gmail.com>
2018-06-22 20:43:03 +0530

    Fix: crash issue

--- Decred Wallet/View Controller/WaiterScreenViewController.swift
+++ Decred Wallet/View Controller/WaiterScreenViewController.swift
@@ -18,7 +18,9 @@ protocol WaiterScreenProtocol {
 
 class WaiterScreenViewController: UIViewController, WaiterScreenProtocol {
     var onTapAnimation: (() -> Void)?
-    
+//    private lazy var doubleTap: UITapGestureRecognizer = {
+//        let tap: UITapGestureRecognizer = UITapGestureRecognizer.init(target: <#T##Any?#>, action: <#T##Selector?#>)
+//    }()
     
     var onFinish:(()->Void)?
     var timer: Timer?
@@ -46,12 +48,11 @@ class WaiterScreenViewController: UIViewController, WaiterScreenProtocol {
     
     @IBAction func goToSeetings(_ sender: Any) {
         timer?.invalidate()
-        onTapAnimation!()
+        onTapAnimation?()
     }
-    
     
     func stopAnimation() {
         logo.image = nil
-        onFinish!()
+        onFinish?()
     }
 }
