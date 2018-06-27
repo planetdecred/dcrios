f4343abe037e2dcf5ba67ecca25c0cde12e8ec26
rktr09 <39329216+rktr09@users.noreply.github.com>
2018-06-27 20:39:51 +0530

    Send Completed popup screen design (#30)
    * Add: Controller to show payment confirmation popup
    
    * Add: Storyboard and controller to show transaction complete popup

---
+++ Decred Wallet/View Controller/SendCompletedViewController/SendCompletedViewController.swift
@@ -0,0 +1,33 @@
+//  SendCompletedViewController.swift
+//  Decred Wallet
+//  Copyright © 2018 The Decred developers. All rights reserved.
+
+import UIKit
+
+class SendCompletedViewController: UIViewController {
+    @IBOutlet private weak var labelTransactionHash: UILabel!
+    var transactionHash: String?
+    
+    var openDetails: (() -> Void)?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        labelTransactionHash.text = transactionHash
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func openAction(_ sender: UIButton) {
+        openDetails?()
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func closeAction(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
+    }
+}
