6c32325135b23c2a9f7e5e4cd8f0466d721d7128
rktr09 <39329216+rktr09@users.noreply.github.com>
2018-06-27 20:39:51 +0530

    Send Completed popup screen design (#30)
    * Add: Controller to show payment confirmation popup
    
    * Add: Storyboard and controller to show transaction complete popup

--- Decred Wallet/View Controller/SendViewController.swift
+++ Decred Wallet/View Controller/SendViewController.swift
@@ -7,7 +7,7 @@
 import UIKit
 
 class SendViewController: UIViewController {
-    
+
     @IBOutlet weak var accountDropdown: DropMenuButton!
     @IBOutlet weak var totalAmountSending: UILabel!
     @IBOutlet weak var estimateFee: UILabel!
@@ -15,24 +15,24 @@ class SendViewController: UIViewController {
     @IBOutlet weak var walletAddress: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         self.accountDropdown.backgroundColor = UIColor.clear
         accountDropdown.initMenu(["My Wallet [153.0055 DCR]", "My Wallet2 [153.0055 DCR]", "My Wallet3 [153.0055 DCR]"], actions: ({ (ind, val) -> (Void) in
-            
+
             self.accountDropdown.setAttributedTitle(self.getAttributedString(str: val), for: UIControlState.normal)
-           
+
             self.accountDropdown.backgroundColor = UIColor(red: 173.0/255.0, green: 231.0/255.0, blue: 249.0/255.0, alpha: 1.0)
         }))
     }
-    
+
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         self.setNavigationBarItem()
          self.navigationItem.title = "Send"
     }
-  
+
     func getAttributedString(str: String) -> NSAttributedString {
-        
+
         let stt = str as NSString!
         let atrStr = NSMutableAttributedString(string: stt! as String)
         let dotRange = stt?.range(of: "[")
@@ -45,7 +45,7 @@ class SendViewController: UIViewController {
                                 range: NSRange(
                                     location:0,
                                     length:(dotRange?.location)!))
-            
+
             atrStr.addAttribute(NSAttributedStringKey.font,
                                 value: UIFont(
                                     name: "Helvetica",
@@ -53,32 +53,32 @@ class SendViewController: UIViewController {
                                 range: NSRange(
                                     location:(dotRange?.location)!,
                                     length:(str.length - (dotRange?.location)!)))
-            
+
             atrStr.addAttribute(NSAttributedStringKey.foregroundColor,
                                 value: UIColor.darkGray,
                                 range: NSRange(
                                     location:0,
                                     length:str.length))
-            
+
         }
         return atrStr
     }
-    
+
     @IBAction func accountDropdown(_ sender: Any) {
     }
-    
+
     @IBAction private func sendFund(_ sender: Any) {
         let storyboard = UIStoryboard(name: "Main", bundle: nil)
         let confirmSendFundViewController = storyboard.instantiateViewController(withIdentifier: "ConfirmToSendFundViewController") as! ConfirmToSendFundViewController
         confirmSendFundViewController.modalTransitionStyle = .crossDissolve
         confirmSendFundViewController.modalPresentationStyle = .overCurrentContext
         confirmSendFundViewController.amount = 25.869
-        
+
         confirmSendFundViewController.confirm = { [weak self] in
             guard let `self` = self else { return }
             debugPrint(self)
         }
-        
+
         self.present(confirmSendFundViewController, animated: true, completion: nil)
     }
 }
