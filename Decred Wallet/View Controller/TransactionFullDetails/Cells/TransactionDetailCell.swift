22cc1329a62d1650c621ad6481c4a3bf0d446a34
Rohit <rktr09@gmail.com>
2018-06-28 19:07:01 +0530

    Fix: Send page UI issue

---
+++ Decred Wallet/View Controller/TransactionFullDetails/Cells/TransactionDetailCell.swift
@@ -0,0 +1,34 @@
+//  TransactionDetailCell.swift
+//  Decred Wallet
+//  Copyright © 2018 The Decred developers. All rights reserved.
+
+import UIKit
+
+class TransactionDetailCell: UITableViewCell {
+    @IBOutlet private weak var labelTitle: UILabel!
+    @IBOutlet private weak var labelValue: UILabel!
+    var txnDetails: TransactionDetails? {
+        didSet {
+            showData()
+        }
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        
+        // Configure the view for the selected state
+    }
+    
+    private func showData() {
+        guard let txn = self.txnDetails else { return }
+        
+        self.labelTitle.text = txn.title
+        self.labelValue.text = txn.value
+        self.labelValue.textColor = txn.textColor
+    }
+}
---
+++ Decred Wallet/View Controller/TransactionFullDetails/Cells/TransactiontOutputDetailsCell.swift
@@ -0,0 +1,29 @@
+//  TransactiontOutputDetailsTableViewCell.swift
+//  Decred Wallet
+//  Copyright © 2018 The Decred developers. All rights reserved.
+
+import UIKit
+
+class TransactiontOutputDetailsCell: UITableViewCell {
+    @IBOutlet private weak var viewContainer: UIView!
+    
+    var expandOrCollapse: (() -> Void)?
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    
+    @IBAction func hideOrExpandAction(_ sender: UIButton) {
+        self.viewContainer.isHidden.toggle()
+        expandOrCollapse?()
+    }
+    
+}
