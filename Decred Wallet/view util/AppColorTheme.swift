400746fb43675e9bc12db10de10e36720707f7d1
Rohit <rktr09@gmail.com>
2018-07-12 14:48:50 +0530

    Remove redundant private label from computed properties

--- Decred Wallet/view util/AppColorTheme.swift
+++ Decred Wallet/view util/AppColorTheme.swift
@@ -7,25 +7,25 @@ enum Theme: Int {
 }
 
 private extension Theme {
-    private struct LightTheme {
+    struct LightTheme {
         let backgroundColor = #colorLiteral(red: 0.9568627451, green: 0.9647058824, blue: 0.9647058824, alpha: 1)
         let textColor = #colorLiteral(red: 0.04705882353, green: 0.1176470588, blue: 0.2431372549, alpha: 1)
         let dimTextColor = #colorLiteral(red: 0.768627451, green: 0.7960784314, blue: 0.8235294118, alpha: 1)
         let navigationBarColor = #colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)
     }
 
-    private struct DarkTheme {
+    struct DarkTheme {
         let backgroundColor = #colorLiteral(red: 0.501960814, green: 0.501960814, blue: 0.501960814, alpha: 1)
         let textColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
         let dimTextColor = #colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)
         let navigationBarColor = #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1)
     }
 
-    private var darkColors: DarkTheme {
+    var darkColors: DarkTheme {
         return DarkTheme()
     }
 
-    private var lightColors: LightTheme {
+    var lightColors: LightTheme {
         return LightTheme()
     }
 }
