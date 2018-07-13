749302ccb38c88a95d6906627ac76810ca285790
Abubakar Suleiman <abubakars29@yahoo.com>
2018-07-13 17:17:58 +0100

    User interface Update for save seed page (#38)

--- Decred Wallet/View Controller/SeedCheckupViewController.swift
+++ Decred Wallet/View Controller/SeedCheckupViewController.swift
@@ -15,12 +15,15 @@ protocol SeedCheckupProtocol {
 class SeedCheckupViewController: UIViewController, SeedCheckupProtocol {
     var seedToVerify: String?
     
+    @IBOutlet weak var txtInputView: UIView!
     @IBOutlet weak var btnConfirm: UIButton!
     @IBOutlet weak var txSeedCheckCombined: UITextView!
     @IBOutlet weak var tfSeedCheckWord: DropDownSearchField!
+    @IBOutlet weak var btnDelete: UIButton!
     override func viewDidLoad() {
         
         super.viewDidLoad()
+        addAccessory()
         let arr = seedToVerify?.components(separatedBy: " ")
         tfSeedCheckWord.itemsToSearch = arr
         tfSeedCheckWord.dropDownListPlaceholder = view
@@ -29,6 +32,10 @@ class SeedCheckupViewController: UIViewController, SeedCheckupProtocol {
             self.tfSeedCheckWord.clean()
             self.btnConfirm.isEnabled = (self.txSeedCheckCombined.text == "\(self.seedToVerify ?? "") ")
         }
+    }
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        tfSeedCheckWord.becomeFirstResponder()
     }
 
     @IBAction func onDelete(_ sender: Any) {
@@ -44,6 +51,62 @@ class SeedCheckupViewController: UIViewController, SeedCheckupProtocol {
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         var vc = segue.destination as! SeedCheckupProtocol
         vc.seedToVerify = seedToVerify
+    }
+    
+    // Input views
+    func addAccessory() {
+        
+        
+        let customView = UIView(frame: CGRect(x: 0, y: 0, width: 0, height: 50))
+        customView.addSubview(txtInputView)
+        customView.backgroundColor = UIColor.red
+        tfSeedCheckWord.inputAccessoryView = customView
+        
+        txtInputView.translatesAutoresizingMaskIntoConstraints = false
+        customView.translatesAutoresizingMaskIntoConstraints = false
+        btnConfirm.translatesAutoresizingMaskIntoConstraints = false
+        btnDelete.translatesAutoresizingMaskIntoConstraints = false
+        
+        
+        // Input view constraints
+        NSLayoutConstraint.activate([
+            txtInputView.leadingAnchor.constraint(equalTo:
+                customView.leadingAnchor, constant: 0),
+            txtInputView.topAnchor.constraint(equalTo:
+                customView.topAnchor, constant: 0),
+            txtInputView.trailingAnchor.constraint(equalTo:
+                customView.trailingAnchor, constant: 0),
+            txtInputView.bottomAnchor.constraint(equalTo:
+                customView.bottomAnchor, constant: 0),
+            ])
+        
+        
+        
+        // Button clear constraints
+        NSLayoutConstraint.activate([
+            btnDelete.leadingAnchor.constraint(equalTo:
+                txtInputView.leadingAnchor, constant: 10),
+            btnDelete.heightAnchor.constraint(equalToConstant: 35),
+            btnDelete.widthAnchor.constraint(equalToConstant: 118),
+            btnDelete.topAnchor.constraint(equalTo:
+                txtInputView.topAnchor, constant: 10)
+            ])
+        
+        // Button confirm constraints
+        NSLayoutConstraint.activate([
+            btnConfirm.rightAnchor.constraint(equalTo:
+                txtInputView.rightAnchor, constant: -10),
+            btnConfirm.heightAnchor.constraint(equalToConstant: 35),
+            btnConfirm.widthAnchor.constraint(equalToConstant: 118),
+            btnConfirm.topAnchor.constraint(equalTo:
+                txtInputView.topAnchor, constant: 10)
+            ])
+        
+        
+    }
+    func enableButton() {
+        btnConfirm.isEnabled = true
+        btnConfirm.alpha = 1.0
     }
     
 
