3e1fca887564aff0c8b93c31e348202c3ffe8d83
Rohit <rktr09@gmail.com>
2018-08-10 20:04:59 +0530

    Show generated seed on view

--- decred_wallet/view_controller/GeneratedSeedDisplayViewController.swift
+++ decred_wallet/view_controller/GeneratedSeedDisplayViewController.swift
@@ -10,14 +10,14 @@ import UIKit
 import Wallet
 
 class GeneratedSeedDisplayViewController: UIViewController {
-
-    @IBOutlet weak var vWarningIcon: UIView!
-    @IBOutlet weak var vWarningLabel: UILabel!
-
-    var seed : String! = ""
+    @IBOutlet var vWarningLabel: UILabel!
+    @IBOutlet private var seedWordLabels: [UILabel]!
     
-    @IBOutlet weak var seedContainer: UIView!
-    var arrWords : Array<String> = []
+    @IBOutlet private var outerStackView: UIStackView!
+    @IBOutlet var seedContainer: UIView!
+    
+    var seed: String! = ""
+    var arrWords: Array<String> = []
     var yPosition: CGFloat?
     var xPostiion: CGFloat?
     var totalWidth: CGFloat?
@@ -30,49 +30,48 @@ class GeneratedSeedDisplayViewController: UIViewController {
         super.viewDidLoad()
         seed = AppContext.instance.decrdConnection?.generateSeed() as String?
         arrWords = (seed?.components(separatedBy: " "))!
-        vWarningLabel.layer.borderColor = GlobalConstants.Colors.orangeColor.cgColor
-        vWarningIcon.layer.borderColor = GlobalConstants.Colors.orangeColor.cgColor
-        vWarningLabel.superview?.layer.borderColor = GlobalConstants.Colors.orangeColor.cgColor
-
-        self.drawSeed()
+        
+        drawSeed()
     }
     
     // MARK: - Utility
     
     func setUpUItraits() {
-        self.totalWidth = seedContainer.frame.size.width
-        self.yPosition = 20.0
-        self.xPostiion = 20.0
-        self.widthOffset = 0.0
-        self.heightOffset = 0.0
-        self.calcultaedHeight = 21.0
+        totalWidth = seedContainer.frame.size.width
+        yPosition = 20.0
+        xPostiion = 20.0
+        widthOffset = 0.0
+        heightOffset = 0.0
+        calcultaedHeight = 21.0
     }
     
     // Draw seed
     func drawSeed() {
-        for view in seedContainer.subviews{
+        for view in seedContainer.subviews {
             view.removeFromSuperview()
         }
-        self.setUpUItraits()
-        for word in arrWords {
-            let wordSize = self.getWidth(str: " " + word + " ")
-            let rect = self.getLocation(stringSize: wordSize.width)
-            let lbl = UILabel(frame: rect)
-            lbl.backgroundColor = GlobalConstants.Colors.lightBlue
-            lbl.font = self.font
-            lbl.clipsToBounds = true
-            lbl.layer.cornerRadius = 3
-            lbl.textAlignment = NSTextAlignment.center
-            lbl.text = word
-            seedContainer.addSubview(lbl)
+        
+        setUpUItraits()
+        
+        seedContainer.addSubview(outerStackView)
+        
+        for count in 0 ..< arrWords.count {
+            seedWordLabels[count].text = "\(count + 1) .\(arrWords[count])"
         }
+        
+//        outerStackView.topAnchor.constraint(equalTo: seedContainer.topAnchor)
+//        outerStackView.bottomAnchor.constraint(equalTo: seedContainer.bottomAnchor)
+//        outerStackView.leadingAnchor.constraint(equalTo: seedContainer.leadingAnchor)
+//        outerStackView.trailingAnchor.constraint(equalTo: seedContainer.trailingAnchor)
+        
+        view.setNeedsLayout()
     }
     
     // Get location for new seed word
     func getLocation(stringSize: CGFloat) -> CGRect {
         let pos = xPostiion! + stringSize
-        let rect : CGRect
-        if( pos < totalWidth! ) {
+        let rect: CGRect
+        if pos < totalWidth! {
             rect = CGRect(origin: CGPoint(x: xPostiion!, y: yPosition!), size: CGSize(width: stringSize, height: 21))
             xPostiion = xPostiion! + stringSize + 5
         } else {
@@ -82,7 +81,6 @@ class GeneratedSeedDisplayViewController: UIViewController {
             xPostiion = xPostiion! + stringSize + 5
         }
         return rect
-        
     }
     
     // Get width for new word
@@ -92,17 +90,12 @@ class GeneratedSeedDisplayViewController: UIViewController {
         let expectedLabelSize = contentNSString.boundingRect(with: maxLabelSize, options: NSStringDrawingOptions.usesLineFragmentOrigin, attributes: [NSAttributedStringKey.font: self.font], context: nil)
         print("\(expectedLabelSize)")
         return expectedLabelSize.size
-        
-        vWarningLabel.layer.borderColor = UIColor(hex: "fd714a").cgColor
-        vWarningIcon.layer.borderColor = UIColor(hex: "fd714a").cgColor
-
     }
     
     // MARK: - Navigation
-
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         var vc = segue.destination as! SeedCheckupProtocol
         vc.seedToVerify = seed
     }
-
 }
