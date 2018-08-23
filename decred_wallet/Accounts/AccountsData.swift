de539f1c8954326ce1a8390f78529f9193972ce8
Rohit <rktr09@gmail.com>
2018-08-23 20:17:47 +0530

    Remove color from accounts

--- decred_wallet/Accounts/AccountsData.swift
+++ decred_wallet/Accounts/AccountsData.swift
@@ -6,12 +6,13 @@ import Foundation
 import UIKit
 
 struct AccountsData {
-    let color: UIColor
+    let color: UIColor?
     let spendableBalance: Double
     let title: String
     let totalBalance: Double
     var isExpanded: Bool = false
-    init(entity:AccountsEntity, color: UIColor){
+
+    init(entity: AccountsEntity, color: UIColor?) {
         self.color = color
         self.spendableBalance = Double((entity.Balance?.dcrSpendable)!)
         self.totalBalance = Double((entity.Balance?.dcrTotal)!)
