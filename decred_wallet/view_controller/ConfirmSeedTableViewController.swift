cb898bfc1b63786993600a588af458d2379b28cf
Philipp Maluta <fil.the.ensoreus@gmail.com>
2018-11-22 19:18:46 +0200

    fixed popup panel with suggestions; adjusted color and font of suggestions text; clean up the code

--- decred_wallet/view_controller/ConfirmSeedTableViewController.swift
+++ decred_wallet/view_controller/ConfirmSeedTableViewController.swift
@@ -12,7 +12,6 @@ class ConfirmSeedTableViewController: UIViewController, UITableViewDelegate, UIT
 
     @IBOutlet var tableView: UITableView?
     
-    @IBOutlet weak var alcVisibleTableHeight: NSLayoutConstraint!
     var allWords: [String]?
     var svSuggestions: UIToolbar?
     var seedWords: [String?] = []
@@ -51,10 +50,7 @@ class ConfirmSeedTableViewController: UIViewController, UITableViewDelegate, UIT
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        registerObserverForKeyboardNotification()
-        resetSuggestions()
         tableView?.dataSource = self
-        hideSuggestions()
         allWords = loadSeedWordsList()
     }
     
@@ -69,41 +65,14 @@ class ConfirmSeedTableViewController: UIViewController, UITableViewDelegate, UIT
         tableView?.scrollToRow(at: IndexPath(row: currentSeedIndex, section: 0), at: .bottom, animated: true)
     }
     
-    @IBAction func onCommitSeedWord(_ sender: Any) {
-        //let word = tfSeed?.text
-//        seedWords.append(word)
-//        textFields[currentSeedIndex]?.text = word
+    func onCommitSeedWord(text:String) {
+        seedWords.append(text)
+        textFields[currentSeedIndex]?.text = text
         currentSeedIndex += 1
-        //tfSeed?.text = ""
         tableView?.reloadData()
         if currentSeedIndex < 33{
             tableView?.scrollToRow(at: IndexPath(row: currentSeedIndex, section: 0), at: .bottom, animated: true)
         }
-    }
-    
-    deinit {
-        unregisterObserverForKeyboardNotification()
-    }
-    
-    func registerObserverForKeyboardNotification(){
-        NotificationCenter.default.addObserver(self, selector: #selector(onKeyboardWillShow), name: NSNotification.Name.UIKeyboardWillShow, object: nil)
-        NotificationCenter.default.addObserver(self, selector: #selector(onKeyboardWillHide), name: NSNotification.Name.UIKeyboardWillHide, object: nil)
-    }
-    
-    func unregisterObserverForKeyboardNotification(){
-        NotificationCenter.default.removeObserver(self, name:NSNotification.Name.UIKeyboardWillShow, object:nil)
-        NotificationCenter.default.removeObserver(self, name:NSNotification.Name.UIKeyboardWillHide, object:nil)
-    }
-    
-    @objc func onKeyboardWillShow(_ notification: Notification){
-        let notificationInfo = notification.userInfo
-        let keyboardFrame = (notificationInfo?[UIKeyboardFrameEndUserInfoKey] as! NSValue).cgRectValue
-        tableView?.contentOffset = CGPoint(x:0, y:keyboardFrame.height / 2)
-        adjustTableHeight(withKeyboard: keyboardFrame.size.height)
-    }
-    
-    @objc func onKeyboardWillHide(_ notification: Notification){
-        tableView?.contentOffset = CGPoint(x:0, y:0)
     }
     
     // MARK: - Table view data source
@@ -129,40 +98,11 @@ class ConfirmSeedTableViewController: UIViewController, UITableViewDelegate, UIT
         return cell!
     }
     
-    private func resetSuggestions(){
-//        let labelWidth = self.view.frame.size.width / 3
-//        svSuggestions = UIToolbar(frame: CGRect(x:0.0, y:0.0, width:self.view.frame.size.width, height:40.0))
-//        svSuggestions?.backgroundColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
-//        suggestionLabel1 = UILabel(frame: CGRect(x: 0, y: 0, width: labelWidth, height: 40))
-//        suggestionLabel2 = UILabel(frame: CGRect(x: 0, y: 0, width: labelWidth, height: 40))
-//        suggestionLabel3 = UILabel(frame: CGRect(x: 0, y: 0, width: labelWidth, height: 40))
-//        suggestionLabel1?.textColor = #colorLiteral(red: 0.1764705926, green: 0.4980392158, blue: 0.7568627596, alpha: 1)
-//        suggestionLabel2?.textColor = #colorLiteral(red: 0.1764705926, green: 0.4980392158, blue: 0.7568627596, alpha: 1)
-//        suggestionLabel3?.textColor = #colorLiteral(red: 0.1764705926, green: 0.4980392158, blue: 0.7568627596, alpha: 1)
-//        suggestionLabel1?.font = UIFont(name: "Source Sans Pro", size: 16.0)
-//        suggestionLabel2?.font = UIFont(name: "Source Sans Pro", size: 16.0)
-//        suggestionLabel3?.font = UIFont(name: "Source Sans Pro", size: 16.0)
-//        let suggestion1 = UIBarButtonItem(title: "label1", style: .plain, target: self, action: #selector(self.pickSuggestion1))
-//        let suggestion2 = UIBarButtonItem(title: "label2", style: .plain, target: self, action: #selector(self.pickSuggestion2))
-//        let suggestion3 = UIBarButtonItem(title: "label3", style: .plain, target: self, action: #selector(self.pickSuggestion3))
-//        suggestion1.customView = suggestionLabel1
-//        suggestion2.customView = suggestionLabel2
-//        suggestion3.customView = suggestionLabel3
-//        let tap1 = UITapGestureRecognizer(target: self, action: #selector(pickSuggestion1))
-//        let tap2 = UITapGestureRecognizer(target: self, action: #selector(pickSuggestion2))
-//        let tap3 = UITapGestureRecognizer(target: self, action: #selector(pickSuggestion3))
-//        suggestionLabel1?.addGestureRecognizer(tap1)
-//        suggestionLabel2?.addGestureRecognizer(tap2)
-//        suggestionLabel3?.addGestureRecognizer(tap3)
-//        suggestionLabel1?.isUserInteractionEnabled = true
-//        suggestionLabel2?.isUserInteractionEnabled = true
-//        suggestionLabel3?.isUserInteractionEnabled = true
-//        suggestionLabel1?.adjustsFontSizeToFitWidth = true
-//        suggestionLabel2?.adjustsFontSizeToFitWidth = true
-//        suggestionLabel3?.adjustsFontSizeToFitWidth = true
-//        svSuggestions!.items = [suggestion1, suggestion2, suggestion3]
-        //vSuggestionsPanel.addSubview(svSuggestions!)
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        currentSeedIndex = indexPath.row
+        performSegue(withIdentifier: "showSuggestionsPopover", sender: indexPath)
     }
+
     
     private func checkupSeed(){
         let seed = seedWords.reduce("", { x, y in  x + " " + y!})
@@ -172,54 +112,24 @@ class ConfirmSeedTableViewController: UIViewController, UITableViewDelegate, UIT
         }
     }
     
-//    @objc func pickSuggestion1(){
-//        if suggestions.count > 0 {
-//            tfSeed?.text = suggestions[0]
-//            suggestions = ["","",""]
-//            hideSuggestions()
-//        }
-//    }
-//
-//    @objc func pickSuggestion2(){
-//        if suggestions.count > 1 {
-//            tfSeed?.text = suggestions[1]
-//            suggestions = ["","",""]
-//            hideSuggestions()
-//        }
-//    }
-//
-//    @objc func pickSuggestion3(){
-//        if suggestions.count > 2 {
-//            tfSeed?.text = suggestions[2]
-//            suggestions = ["","",""]
-//            hideSuggestions()
-//        }
-//    }
-//
-    private func adjustTableHeight(withKeyboard height:CGFloat){
-        let totalHeight = view.frame.size.height
-        alcVisibleTableHeight.constant = totalHeight - 100.0 - height
-        tableView?.scrollRectToVisible(CGRect(x: 0.0, y: 0.0, width: 100, height: 44), animated: false)
-    }
-    
-    private func showSuggestions(){
-        //vSuggestionsPanel.isHidden = false
-    }
-    
-    private func hideSuggestions(){
-        //vSuggestionsPanel.isHidden = true
-    }
     
     private func loadSeedWordsList() -> [String]{
         let seedWordsPath = Bundle.main.path(forResource: "wordlist", ofType: "txt")
         let seedWords = try? String(contentsOfFile: seedWordsPath ?? "")
         return seedWords?.split{$0 == "\n"}.map(String.init) ?? []
     }
-}
-
-extension ConfirmSeedTableViewController : UITextFieldDelegate{
     
-    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "showSuggestionsPopover" {
+            let popup = segue.destination as? SeedSuggestionsViewController
+            popup?.suggestions = prepareSuggestions(for: (sender as! IndexPath).row)
+            popup?.onSuggestionPicked = {(text) in
+                self.onCommitSeedWord(text: text)
+            }
+        }
+    }
+    
+    private func prepareSuggestions(for row:Int) -> [String]{
         var suggestionsWithFake: [String] = ["","",""]
         let trueSeedIndex = Int.random(in: 0...2)
         suggestionsWithFake[trueSeedIndex] = seedToVerify[currentSeedIndex]
@@ -231,23 +141,6 @@ extension ConfirmSeedTableViewController : UITextFieldDelegate{
                 fakeIndex += 1
             }
         }
-        
-        self.suggestions = suggestionsWithFake
-//        self.suggestions = seedToVerify.filter({
-//            return ($0.lowercased().hasPrefix((textField.text! + string).lowercased()) && (textField.text?.count)! >= 1)
-//        })
-        if suggestions.count > 0 {
-            showSuggestions()
-        }else{
-            hideSuggestions()
-        }
-        return true
-    }
-    
-    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-        onCommitSeedWord(textField)
-        suggestions = []
-        hideSuggestions()
-        return true
+        return  suggestionsWithFake
     }
 }
--- decred_wallet/view_controller/SeedSuggestionsViewController.swift
+++ decred_wallet/view_controller/SeedSuggestionsViewController.swift
@@ -7,28 +7,31 @@
 
 import UIKit
 
-class SeedSuggestionsViewController: UIViewController {
+class SeedSuggestionsViewController: UIViewController, UITabBarDelegate {
     
     var suggestions = ["","",""]
     @IBOutlet var suggestion1: UITabBarItem!
     @IBOutlet var suggestion2: UITabBarItem!
     @IBOutlet var suggestion3: UITabBarItem!
-    
+    var onSuggestionPicked: ((String)->Void)?
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        suggestion1.setTitleTextAttributes([.font : UIFont(name: "Source Sans Pro", size: 16.0)!, .foregroundColor: #colorLiteral(red: 0.1764705926, green: 0.4980392158, blue: 0.7568627596, alpha: 1)], for: .normal)
+        suggestion2.setTitleTextAttributes([.font : UIFont(name: "Source Sans Pro", size: 16.0)!, .foregroundColor: #colorLiteral(red: 0.1764705926, green: 0.4980392158, blue: 0.7568627596, alpha: 1)], for: .normal)
+        suggestion3.setTitleTextAttributes([.font : UIFont(name: "Source Sans Pro", size: 16.0)!, .foregroundColor: #colorLiteral(red: 0.1764705926, green: 0.4980392158, blue: 0.7568627596, alpha: 1)], for: .normal)
+        suggestion1.title = suggestions[0]
+        suggestion2.title = suggestions[1]
+        suggestion3.title = suggestions[2]
     }
-    */
+    
+    func tabBar(_ tabBar: UITabBar, didSelect item: UITabBarItem){
+        onSuggestionPicked?(item.title!)
+        dismiss(animated: true, completion: nil)
+    }
 
 }
