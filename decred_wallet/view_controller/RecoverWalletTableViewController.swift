ac202b411cbeaf45529dc1f0ca90837e41284767
Philipp Maluta <fil.the.ensoreus@gmail.com>
2018-11-27 16:13:09 +0200

    stick confirm buttons to  bottom of screen

--- decred_wallet/view_controller/RecoverWalletTableViewController.swift
+++ decred_wallet/view_controller/RecoverWalletTableViewController.swift
@@ -8,9 +8,10 @@
 
 import UIKit
 
-class RecoverWalletTableViewController: UITableViewController {
-    var svSuggestions: UIToolbar?
+class RecoverWalletTableViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    @IBOutlet var tableView : UITableView!
     
+    var svSuggestions: UIToolbar?
     var seedWords: [String?] = []
     var suggestionLabel1 : UILabel?
     var suggestionLabel2 : UILabel?
@@ -30,15 +31,15 @@ class RecoverWalletTableViewController: UITableViewController {
     
     // MARK: - Table view data source
     
-    override func numberOfSections(in tableView: UITableView) -> Int {
+    func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
     
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 33
     }
     
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "seedWordCell", for: indexPath) as? RecoveryWalletSeedWordsCell
         cell?.setup(wordNum: indexPath.row, word: seedWords.count <= indexPath.row ? "" : seedWords[indexPath.row] ?? "", seed: seedtmp)
         cell?.tfSeedWord.isEnabled = (indexPath.row == 0 || textFields.count < indexPath.row )
