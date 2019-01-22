3ea609f9c8c987b353023b8fe768a588e503ca85
ensoreus <fil.the.ensoreus@gmail.com>
2019-01-22 02:02:46 +0200

    Recover wallet: seed words entering with dropdown  (#128)
    * Recover wallet: seed  word list UI
    
    * adjusted header view
    
    * RecoveryWallet:Customized cell with seed validator
    
    * RecoverWallet: suggestions panel; filling suggestions panel with matched seed words
    
    * Recover Wallet: filtering wordlist, filling in suggestions list.
    
    * RecoverWallet:fixed suggestions input
    
    * RecoverWallet: fall down through textfields on Next button
    
    * RecoverWallet: confirm seed for verifications; back button
    
    * RecoverWallet:changed suggestions font according to design
    
    * adopted for wide screens
    
    * added missed segue for recover button
    
    * added popup as separate view controller; customized suggestions font
    
    * attached new suggestion panel's logic to RecoverWalletSeed controller: popup invocation, set position in orer to appear right under current editable textfield, return picked up suggestion
    
    * bottom panel with clear and confirm buttons
    
    * stick confirm buttons to  bottom of screen
    
    * checkup seed onConfirm button
    
    * fixing input blocking by suggestions panel
    
    * made vertical drop down for suggestion
    
    * make Dropdown float over the tableview
    
    * pass focus to next seed field; seed buttons appearance
    
    * debugging floating dropdown
    
    * fixed dropdown slide and tracking active textfields
    
    * dropdown slide in correction according to keydoard appearance
    
    * fixing floating dropdown appearance while scrolling down to the end of seeds list
    
    * fixed dropdown size and it's direction at the list
    
    * cut down dropdown list to4 items
    
    * fixed segue crash; fixed seed pass to security screen
    
    * adjusted dropdown menu at first cell
    
    * fixed dropdown placing on top and on the buttom
    
    * fixing dropdown positioning
    
    * fixed dropdown layout

---
+++ decred_wallet/view_controller/TransactionFullDetails/Cells/RecoveryWalletSeedWordsCell.swift
@@ -0,0 +1,48 @@
+//
+//  RecoveryWalletSeedWordsCell.swift
+//  Decred Wallet
+//
+// Copyright (c) 2018, The Decred developers
+// See LICENSE for details.
+//
+
+import UIKit
+
+class RecoveryWalletSeedWordsCell: UITableViewCell, UITextFieldDelegate {
+    
+    @IBOutlet weak var lbWordNum: UILabel!
+    @IBOutlet weak var tfSeedWord: DropDownSearchField!
+    
+    var seed:[String]?
+    var wordNum:Int = 0
+    var onPickUpSeed:((Int, String)->Void)?{
+        set{
+            tfSeedWord.onSelect = newValue
+        }
+        get{
+            return tfSeedWord.onSelect
+        }
+    }
+    
+    func setup(wordNum:Int, word: String?, seed:[String], placeholder: UIView){
+        tfSeedWord.dropDownListPlaceholder = placeholder
+        lbWordNum.text = "Word #\(wordNum + 1)"
+        tfSeedWord.text = word ?? ""
+        self.seed = seed
+        self.wordNum = wordNum
+        tfSeedWord.autocorrectionType = .no
+        tfSeedWord.itemsToSearch = seed
+        tfSeedWord.vertPosition = self.frame.origin.y
+        tfSeedWord.setupDropdownTable()
+    }
+    
+    
+    func hideDropDown(){
+        tfSeedWord.hideDropDown()
+        tfSeedWord.clean()
+    }
+    
+    func updatePlaceholder(vertPosition: Int){
+        tfSeedWord.updatePlaceholder(position:vertPosition)
+    }
+}
