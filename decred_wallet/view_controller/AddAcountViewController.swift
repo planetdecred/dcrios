2d1ae5529fd40767ff9c14e778e6374b225e17d7
Abubakar Suleiman <abubakars29@yahoo.com>
2019-02-12 20:37:21 +0100

    Disable default wallet switch on imported account (#179)
    * Disabled default wallet switch for imported account
    
    * Display correct error message on adding account
    
    * Removed overlapping progress dialog

--- decred_wallet/view_controller/AddAcountViewController.swift
+++ decred_wallet/view_controller/AddAcountViewController.swift
@@ -9,6 +9,7 @@
 import UIKit
 
 
+
 class AddAcountViewController: UIViewController {
     @IBOutlet weak var passphrase: UITextField!
     @IBOutlet weak var accountName: UITextField!
@@ -21,17 +22,29 @@ class AddAcountViewController: UIViewController {
     }
     
     @IBAction func createFnc(_ sender: Any) {
+        if (accountName.text?.length)! < 1{
+            Info(msg: "Please input an account name")
+            return
+        }
         let name = accountName.text
         let pass = passphrase.text
         if(!(name!.isEmpty) || (pass!.isEmpty)){
             let finalPassphrase = pass! as NSString
             let finalPassphraseData = finalPassphrase .data(using: String.Encoding.utf8.rawValue)!
+            DispatchQueue.global(qos: .userInitiated).async {
             do {
                try SingleInstance.shared.wallet?.nextAccount(name, privPass: finalPassphraseData)
+                DispatchQueue.main.async {
+              
                 self.dismiss(animated: true, completion: nil)
+                }
                 
             }catch{
+                DispatchQueue.main.async {
+                    self.showError(error: error)
                 print("error")
+                }
+            }
             }
                 
             
@@ -45,5 +58,16 @@ class AddAcountViewController: UIViewController {
         self.dismiss(animated: true, completion: nil)
     }
     
-
+    func showError(error:Error){
+        let alert = UIAlertController(title: "Error Message", message: error.localizedDescription, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default)
+        alert.addAction(okAction)
+        present(alert, animated: true, completion: nil)
+    }
+    func Info(msg:String){
+        let alert = UIAlertController(title: "Info", message: msg, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default)
+        alert.addAction(okAction)
+        present(alert, animated: true, completion: nil)
+    }
 }
