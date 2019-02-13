460e1b4bd866720b243b4b8b9fe5b90b0f9a6e8b
Abubakar Suleiman <abubakars29@yahoo.com>
2019-02-13 23:40:41 +0100

    Add license statement to all source files (#185)

--- decred_wallet/table_view_cell/TransactionHistoryTableViewCell.swift
+++ decred_wallet/table_view_cell/TransactionHistoryTableViewCell.swift
@@ -2,9 +2,10 @@
 //  TransactionHistoryTableViewCell.swift
 //  Decred Wallet
 //
-//  Created by rails on 23/05/18.
-//  Copyright © 2018 The Decred developers. All rights reserved.
-//
+// Copyright (c) 2018-2019 The Decred developers
+// Use of this source code is governed by an ISC
+// license that can be found in the LICENSE file.
+
 
 import UIKit
 
--- decred_wallet/view_util/Data.swift
+++ decred_wallet/view_util/Data.swift
@@ -2,9 +2,9 @@
 //  Data.swift
 //  Decred Wallet
 //
-//  Created by Philipp Maluta on 27.07.2018.
-//  Copyright © 2018 The Decred developers. All rights reserved.
-//
+// Copyright (c) 2018-2019 The Decred developers
+// Use of this source code is governed by an ISC
+// license that can be found in the LICENSE file.
 
 import Foundation
 
