49d3b68507698e33a107ed465a0f5e0ca38c09d6
Suleiman Abubakar <abubakars29@yahoo.com>
2019-02-25 14:27:45 +0100

    UI for syn indicator

--- decred_wallet/SingleInstance.swift
+++ decred_wallet/SingleInstance.swift
@@ -11,6 +11,13 @@ import UIKit
 import Dcrlibwallet
 public class SingleInstance{
     var wallet: DcrlibwalletLibWallet?
+    var  synced = false, syncing = true;
+    var peers = 0;
+    
+    var syncStartPoint = -1, syncCurrentPoint = -1, syncEndPoint = -1;
+    var syncProgress = 0, accountDiscoveryStartTime = 0, totalDiscoveryTime = 0;
+    var fetchHeaderTime = -1, totalFetchTime = -1, rescanTime = 0, syncRemainingTime = 0, initialSyncEstimate = -1;
+    var syncStatus = "", syncVerbose = "";
     public class var shared: SingleInstance {
         struct Static {
             static let instance: SingleInstance = SingleInstance()
