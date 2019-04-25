264c3fc8f5a76a14ac460f8543ccbbe0776d3e42
Suleiman Abubakar <abubakars29@yahoo.com>
2019-04-25 07:49:09 +0100

    code format,comments and readable variables

--- decred_wallet/view_controller/Security/PasswordSetupViewController.swift
+++ decred_wallet/view_controller/Security/PasswordSetupViewController.swift
@@ -11,35 +11,31 @@ import PasswordStrength
 import JGProgressHUD
 
 class PasswordSetupViewController: UIViewController, SeedCheckupProtocol, UITextFieldDelegate, StartUpPasswordProtocol,PinEnteredProtocol{
-    var pinInput: String?
     
-    
-    var senders: String?
-    var seedToVerify: String?
-    var pass_pinToVerify: String?
-    
-    @IBOutlet weak var confirmPassLabel: UILabel!
+   //outlets
+    @IBOutlet weak var confirmPasswordLabel: UILabel!
     @IBOutlet weak var passwordLabel: UILabel!
-    @IBOutlet weak var tfPassword: UITextField!
-    @IBOutlet weak var tfConfirmPassword: UITextField!
-    @IBOutlet weak var lbMatchIndicator: UILabel!
-    @IBOutlet weak var pbPasswordStrength: UIProgressView!
-    @IBOutlet weak var lbPasswordStrengthLabel: UILabel!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var confirmPasswordTextField: UITextField!
+    @IBOutlet weak var passwordMatchIndicatorLabel: UILabel!
+    @IBOutlet weak var passwordStrength: UIProgressView!
+    @IBOutlet weak var passwordStrengthLabel: UILabel!
     @IBOutlet weak var headerText: UILabel!
     @IBOutlet weak var okBtn: UIButton!
     
+    var pinInput: String?
+    var senders: String?
+    var seedToVerify: String?
+    var pass_pinToVerify: String?
     let passwordStrengthMeasurer = MEPasswordStrength()
     var progressHud : JGProgressHUD?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        tfPassword.delegate = self
-        tfConfirmPassword.delegate = self
-        //lbMatchIndicator.isHidden = true
-        // pbPasswordStrength.isHidden = true
-        // lbPasswordStrengthLabel.isHidden = true
+        passwordTextField.delegate = self
+        confirmPasswordTextField.delegate = self
         okBtn.layer.cornerRadius = 5
-        tfConfirmPassword.addTarget(self, action: #selector(textFieldDidChange(_:)), for: .editingChanged)
+        confirmPasswordTextField.addTarget(self, action: #selector(textFieldDidChange(_:)), for: .editingChanged)
         if (self.seedToVerify != nil) {
             senders = "seed"
         }
@@ -47,23 +43,18 @@ class PasswordSetupViewController: UIViewController, SeedCheckupProtocol, UIText
         setHeader()
     }
     
+    // enccrypt wallet action when setting up wallet
     func onEncrypt() {
-        
         progressHud = showProgressHud(with: "creating wallet...")
-        
         let seed = self.seedToVerify!
-        let pass = self.tfPassword!.text
-        
+        let pass = self.passwordTextField!.text
         DispatchQueue.global(qos: .userInitiated).async { [weak self] in
             guard let this = self else { return }
-            
             do {
                 if SingleInstance.shared.wallet == nil {
                     return
                 }
-                
                 let wallet = SingleInstance.shared.wallet!
-                
                 try wallet.createWallet(pass, seedMnemonic: seed)
                 try wallet.unlock(pass!.data(using: .utf8))
                 DispatchQueue.main.async {
@@ -85,13 +76,10 @@ class PasswordSetupViewController: UIViewController, SeedCheckupProtocol, UIText
     
     func SetstartupPin_pas(){
         progressHud = showProgressHud(with: "Securing wallet...")
-        
         let finalkeyData = ("public" as NSString).data(using: String.Encoding.utf8.rawValue)!
-        let finalkeypassData = (self.tfPassword!.text! as NSString).data(using: String.Encoding.utf8.rawValue)!
-        
+        let finalkeypassData = (self.passwordTextField!.text! as NSString).data(using: String.Encoding.utf8.rawValue)!
         DispatchQueue.global(qos: .userInitiated).async { [weak self] in
             guard let this = self else { return }
-            
             do {
                 try SingleInstance.shared.wallet?.changePublicPassphrase(finalkeyData, newPass: finalkeypassData)
                 DispatchQueue.main.async {
@@ -113,13 +101,10 @@ class PasswordSetupViewController: UIViewController, SeedCheckupProtocol, UIText
     
     func ChangeSpendingPass(){
         progressHud = showProgressHud(with: "Changing spending password...")
-        
         let finalkeyData = (pass_pinToVerify! as NSString).data(using: String.Encoding.utf8.rawValue)!
-        let finalkeypassData = (self.tfPassword!.text! as NSString).data(using: String.Encoding.utf8.rawValue)!
-        
+        let finalkeypassData = (self.passwordTextField!.text! as NSString).data(using: String.Encoding.utf8.rawValue)!
         DispatchQueue.global(qos: .userInitiated).async { [weak self] in
             guard let this = self else { return }
-            
             do {
                 try SingleInstance.shared.wallet?.changePrivatePassphrase(finalkeyData, newPass: finalkeypassData)
                 DispatchQueue.main.async {
@@ -137,15 +122,13 @@ class PasswordSetupViewController: UIViewController, SeedCheckupProtocol, UIText
             }
         }
     }
+    
     func ChangeStartupPass(){
         progressHud = showProgressHud(with: "Changing startup password...")
-        
         let finalkeyData = (pass_pinToVerify! as NSString).data(using: String.Encoding.utf8.rawValue)!
-        let finalkeypassData = (self.tfPassword!.text! as NSString).data(using: String.Encoding.utf8.rawValue)!
-        
+        let finalkeypassData = (self.passwordTextField!.text! as NSString).data(using: String.Encoding.utf8.rawValue)!
         DispatchQueue.global(qos: .userInitiated).async { [weak self] in
             guard let this = self else { return }
-            
             do {
                 try SingleInstance.shared.wallet?.changePublicPassphrase(finalkeyData, newPass: finalkeypassData)
                 DispatchQueue.main.async {
@@ -164,54 +147,9 @@ class PasswordSetupViewController: UIViewController, SeedCheckupProtocol, UIText
             }
         }
     }
-    func showError(error:Error){
-        let alert = UIAlertController(title: "Warning", message: error.localizedDescription, preferredStyle: .alert)
-        let okAction = UIAlertAction(title: "OK", style: .default) { (action) in
-            alert.dismiss(animated: true, completion: {self.navigationController?.popToRootViewController(animated: true)})
-        }
-        alert.addAction(okAction)
-        present(alert, animated: true, completion: {self.progressHud!.dismiss()})
-    }
-    
-    @objc func textFieldDidChange(_: NSObject){
-        if self.tfPassword.text == self.tfConfirmPassword.text {
-            self.lbMatchIndicator.textColor = #colorLiteral(red: 0.2537069321, green: 0.8615272641, blue: 0.7028611302, alpha: 1)
-            self.lbMatchIndicator.text = "PASSWORDS MATCH"
-            self.okBtn.isEnabled = true
-            self.okBtn.backgroundColor = UIColor(hex: "#007AFF")
-            self.okBtn.setTitleColor(UIColor.white, for: .normal)
-        }else{
-            self.lbMatchIndicator.textColor = #colorLiteral(red: 0.9254902005, green: 0.2352941185, blue: 0.1019607857, alpha: 1)
-            self.lbMatchIndicator.text = "PASSWORDS DO NOT MATCH"
-            self.okBtn.isEnabled = false
-            self.okBtn.backgroundColor = UIColor(hex: "#E6EAED")
-            self.okBtn.setTitleColor(UIColor(hex: "#000000", alpha: 0.61), for: .normal)
-        }
-    }
-    @IBAction func creayePassBtn(_ sender: Any) {
-        self.ActionButton()
-    }
-    
-    
-    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
-        
-        if (textField.tag == 5) { //password
-            pbPasswordStrength.progress = passwordStrengthMeasurer.strength(forPassword: textField.text) as! Float
-            pbPasswordStrength.progressTintColor = passwordStrengthMeasurer.strengthColor(forPassword: textField.text)
-            pbPasswordStrength.isHidden = false
-            lbPasswordStrengthLabel.isHidden = false
-        } else {
-            lbMatchIndicator.isHidden = false
-        }
-        return true
-    }
-    
-    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-        return self.ActionButton()
-    }
     
     func ActionButton() -> Bool{
-        guard self.tfPassword.text == self.tfConfirmPassword.text else {
+        guard self.passwordTextField.text == self.confirmPasswordTextField.text else {
             self.showMessageDialog(title: "Error", message: "Password does not match")
             return false
         }
@@ -230,10 +168,56 @@ class PasswordSetupViewController: UIViewController, SeedCheckupProtocol, UIText
         return true
     }
     
+    @IBAction func createPassBtn(_ sender: Any) {
+        self.ActionButton()
+    }
+    
+    func showError(error:Error){
+        let alert = UIAlertController(title: "Warning", message: error.localizedDescription, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default) { (action) in
+            alert.dismiss(animated: true, completion: {self.navigationController?.popToRootViewController(animated: true)})
+        }
+        alert.addAction(okAction)
+        present(alert, animated: true, completion: {self.progressHud!.dismiss()})
+    }
+    
+    @objc func textFieldDidChange(_: NSObject){
+        if self.passwordTextField.text == self.confirmPasswordTextField.text {
+            self.passwordMatchIndicatorLabel.textColor = #colorLiteral(red: 0.2537069321, green: 0.8615272641, blue: 0.7028611302, alpha: 1)
+            self.passwordMatchIndicatorLabel.text = "PASSWORDS MATCH"
+            self.okBtn.isEnabled = true
+            self.okBtn.backgroundColor = UIColor(hex: "#007AFF")
+            self.okBtn.setTitleColor(UIColor.white, for: .normal)
+        }else{
+            self.passwordMatchIndicatorLabel.textColor = #colorLiteral(red: 0.9254902005, green: 0.2352941185, blue: 0.1019607857, alpha: 1)
+            self.passwordMatchIndicatorLabel.text = "PASSWORDS DO NOT MATCH"
+            self.okBtn.isEnabled = false
+            self.okBtn.backgroundColor = UIColor(hex: "#E6EAED")
+            self.okBtn.setTitleColor(UIColor(hex: "#000000", alpha: 0.61), for: .normal)
+        }
+    }
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        if (textField.tag == 5) { //password
+            passwordStrength.progress = passwordStrengthMeasurer.strength(forPassword: textField.text) as! Float
+            passwordStrength.progressTintColor = passwordStrengthMeasurer.strengthColor(forPassword: textField.text)
+            passwordStrength.isHidden = false
+            passwordStrengthLabel.isHidden = false
+        } else {
+            passwordMatchIndicatorLabel.isHidden = false
+        }
+        return true
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        return self.ActionButton()
+    }
+    
     func dismissView() {
         self.navigationController?.popToRootViewController(animated: true)
     }
     
+    // Set Text Header according to it current function
     func setHeader(){
         if (senders == "launcher") {
             headerText.text = "Enter Startup Password"
@@ -254,6 +238,8 @@ class PasswordSetupViewController: UIViewController, SeedCheckupProtocol, UIText
             headerText.text = "Create Spending Password"
         }
     }
+    
+    // Detect screen size and set appropriate font size
     func setScreenFont(){
         if UIDevice().userInterfaceIdiom == .phone {
             switch UIScreen.main.nativeBounds.height {
@@ -264,7 +250,6 @@ class PasswordSetupViewController: UIViewController, SeedCheckupProtocol, UIText
             case 1334:
                 // iPhone 6/6S/7/8
                 self.setFontSize(confirmPassLabeTxt: 15, passwordLabelTxt: 15, lbMatchIndicatorTxt: 15, lbPasswordStrengthLabelTxt: 15, okBtnTxt: 15, headerTxt: 20, tfPasswordTxt: 15, tfConfirmPasswordTxt: 15)
-                
                 break
             case 2208:
                 //iPhone 6+/6S+/7+/8+
@@ -273,7 +258,6 @@ class PasswordSetupViewController: UIViewController, SeedCheckupProtocol, UIText
             case 2436:
                 // iPhone X
                 self.setFontSize(confirmPassLabeTxt: 15, passwordLabelTxt: 15, lbMatchIndicatorTxt: 15, lbPasswordStrengthLabelTxt: 15, okBtnTxt: 15, headerTxt: 20, tfPasswordTxt: 15, tfConfirmPasswordTxt: 15)
-                
                 break
             default: break
                 // print("unknown")
@@ -296,20 +280,20 @@ class PasswordSetupViewController: UIViewController, SeedCheckupProtocol, UIText
                 self.setFontSize(confirmPassLabeTxt: 37, passwordLabelTxt: 37, lbMatchIndicatorTxt: 37, lbPasswordStrengthLabelTxt: 37, okBtnTxt: 37, headerTxt: 52, tfPasswordTxt: 37, tfConfirmPasswordTxt: 37)
                 break
             default:
-                print("unknown")
-                print(UIScreen.main.nativeBounds.height)
                 break
             }
         }
     }
+    
+    //set font size for displayed text
     func setFontSize(confirmPassLabeTxt: CGFloat, passwordLabelTxt: CGFloat, lbMatchIndicatorTxt: CGFloat,lbPasswordStrengthLabelTxt: CGFloat, okBtnTxt: CGFloat,headerTxt: CGFloat,tfPasswordTxt: CGFloat, tfConfirmPasswordTxt : CGFloat){
-        self.confirmPassLabel.font = confirmPassLabel.font?.withSize(confirmPassLabeTxt)
+        self.confirmPasswordLabel.font = confirmPasswordLabel.font?.withSize(confirmPassLabeTxt)
         self.passwordLabel.font = passwordLabel.font?.withSize(passwordLabelTxt)
-        self.lbMatchIndicator.font = lbMatchIndicator.font?.withSize(lbMatchIndicatorTxt)
-        self.lbPasswordStrengthLabel.font = lbPasswordStrengthLabel.font.withSize(lbPasswordStrengthLabelTxt)
+        self.passwordMatchIndicatorLabel.font = passwordMatchIndicatorLabel.font?.withSize(lbMatchIndicatorTxt)
+        self.passwordStrengthLabel.font = passwordStrengthLabel.font.withSize(lbPasswordStrengthLabelTxt)
         self.headerText.font = headerText.font.withSize(headerTxt)
         self.okBtn.titleLabel?.font = .systemFont(ofSize: okBtnTxt)
-        self.tfPassword.font = .systemFont(ofSize: tfPasswordTxt)
-        self.tfConfirmPassword.font = .systemFont(ofSize: tfConfirmPasswordTxt)
+        self.passwordTextField.font = .systemFont(ofSize: tfPasswordTxt)
+        self.confirmPasswordTextField.font = .systemFont(ofSize: tfConfirmPasswordTxt)
     }
 }
