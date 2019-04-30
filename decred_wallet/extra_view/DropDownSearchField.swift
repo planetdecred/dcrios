7c9fb8fbf09e74a5aaec5b4882d687b6769db90b
Wisdom Arerosuoghene <wisdom.arerosuoghene@gmail.com>
2019-04-30 07:22:48 +0100

    wallet recovery (seed restore) page improvements (#341)
    * fix issue with sleep/wake hiding views
    
    * in progress
    
    * recover wallet page working as expected
    
    * enable next input after selecting word
    
    * hide keyboard on tap outside textfield
    
    * make page resemble android moer
    
    * cleanup ui
    
    * restore seed page title, hint and error messages
    
    * fix seed restore dropdown behaviour
    
    * add padding to seed word input field
    
    * use custom dropdown for seed word selection
    
    * edit mainnet/testnet schemes
    
    * add hint to seed words drop down
    
    * restore test code

--- decred_wallet/extra_view/DropDownSearchField.swift
+++ decred_wallet/extra_view/DropDownSearchField.swift
@@ -8,143 +8,139 @@
 
 import UIKit
 
-protocol DropDownResultsListProtocol : UITableViewDataSource, UITableViewDelegate {
-    var items: [String]?{get set}
-    var cellIdentifier: String{get set}
-    var onSelect:((Int, String)->Void)? {get set}
+class DropDownSearchField: UITextField, UITextFieldDelegate {
+    var dropDownListPlaceholder: UIView?
+    var wordsToFilter: [String] = []
+    var maxDropDownResultsCount = 4
+    
+    private var dropDownTable: UITableView?
+    private var dropDownTableDataSource: DropDownTableDataSource
+    
+    // callbacks for when user selects a word from dropdown or leaves input field without selecting a word
+    var onTextChanged: (() -> Void)?
+    var onWordSelected:((_ selectedWord: String) -> Void)? {
+        get {
+            return self.dropDownTableDataSource.onWordSelected
+        }
+        set {
+            self.dropDownTableDataSource.onWordSelected = newValue
+        }
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        dropDownTableDataSource = DropDownTableDataSource()
+        super.init(coder: aDecoder)
+    }
+    
+    func setupDropdownTable(with wordsToFilter: [String], and placeHolderView: UIView) {
+        self.wordsToFilter = wordsToFilter
+        self.dropDownListPlaceholder = placeHolderView
+        self.dropDownListPlaceholder!.isHidden = true
+        
+        let dropDownTableRect = CGRect(
+            x: 0, y: 0,
+            width: Int(self.dropDownListPlaceholder!.frame.size.width),
+            height: Int(self.dropDownTableDataSource.tableHeight())
+        )
+        self.dropDownTable = UITableView(frame: dropDownTableRect, style: .plain)
+        
+        self.dropDownTable?.register(UITableViewCell.self, forCellReuseIdentifier: (self.dropDownTableDataSource.cellIdentifier))
+        self.dropDownTable?.dataSource = self.dropDownTableDataSource
+        self.dropDownTable?.delegate = self.dropDownTableDataSource
+        
+        // listen for text editing event and filter words
+        self.addTarget(self, action: #selector(textFieldDidChange(_:)), for: .editingChanged)
+        
+        // listen for when text editing ends and hide dropdown
+        self.delegate = self
+    }
+    
+    @objc func textFieldDidChange(_ textField: UITextField) {
+        let currentText = textField.text ?? ""
+        var filteredWords = self.wordsToFilter.filter({
+            return (currentText.count >= 2 && $0.lowercased().hasPrefix(currentText.lowercased()))
+        })
+        if filteredWords.count > self.maxDropDownResultsCount {
+            filteredWords = Array(filteredWords.dropLast(filteredWords.count - self.maxDropDownResultsCount))
+        }
+        self.dropDownTableDataSource.filteredWords = filteredWords
+        
+        if filteredWords.count == 0 {
+            self.dropDownListPlaceholder?.isHidden = true
+            return
+        }
+        
+        let tableHeight = self.dropDownTableDataSource.tableHeight()
+        let textFieldYPos = textField.superview?.convert(textField.frame.origin, to: nil).y ?? 0
+        let dropDownHolderMaxHeight = self.dropDownListPlaceholder?.superview?.frame.height ?? 0
+        
+        var dropDownYPos = textFieldYPos + textField.frame.height
+        if (dropDownYPos + tableHeight) > dropDownHolderMaxHeight {
+            dropDownYPos = textFieldYPos - tableHeight
+        }
+        
+        self.dropDownTable?.frame.size.height = tableHeight
+        self.dropDownListPlaceholder?.frame.size.height = tableHeight
+        self.dropDownListPlaceholder?.frame.origin.y = dropDownYPos
+        
+        self.dropDownTable?.reloadData()
+        self.dropDownListPlaceholder?.isHidden = false
+        
+        if self.dropDownListPlaceholder?.subviews.count == 0 {
+            self.dropDownListPlaceholder?.addSubview(self.dropDownTable!)
+        }
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField, reason: UITextFieldDidEndEditingReason) {
+        self.dropDownListPlaceholder?.isHidden = true
+        self.dropDownTable?.removeFromSuperview()
+        self.onTextChanged?()
+    }
 }
 
-protocol SearchDataSourceProtocol: class {
-    var itemsToSearch:[String]?{get set}
-    var dropDownListPlaceholder: UIView? {get set}
-    var onTextChanged:(()->Void)? {get set}
-    func clean()
-}
-
-class DropDownListDataSource: NSObject,  DropDownResultsListProtocol {
+class DropDownTableDataSource: NSObject, UITableViewDataSource, UITableViewDelegate {
+    let cellIdentifier: String = "dropDownCell"
+    let cellHeight: CGFloat = 40
+    let footerHeight: CGFloat = 22
     
-    var onSelect: ((Int, String) -> Void)?
-    var items: [String]?
-    var cellIdentifier: String = ""
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return items?.count ?? 0
-    }
-    
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        let index = indexPath.row
-        let item = items?[index] ?? ""
-        onSelect?(index, item)
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath)
-        setup(cell: cell, index: indexPath.row)
-        return cell
-    }
+    var filteredWords: [String] = []
+    var onWordSelected: ((_ selectedWord: String) -> Void)?
     
     func numberOfSectionsInTableView(tableView: UITableView) -> Int {
         return 1
     }
     
-    private func setup(cell:UITableViewCell, index: Int){
-        cell.textLabel?.text = items?[index]
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.filteredWords.count
     }
     
-    @objc func onPickSeedWord(sender: UIButton?){
-        let index = sender!.tag
-        let item = items?[index] ?? ""
-        onSelect?(index, item)
-    }
-}
-
-class DropDownSearchField: UITextField, UITextFieldDelegate, SearchDataSourceProtocol {
-    
-    var onTextChanged: (() -> Void)?
-    var dropDownListPlaceholder: UIView?
-    var itemsToSearch: [String]?
-    private var dropDownTable: UITableView?
-    var searchResult: DropDownResultsListProtocol?
-    var vertPosition: CGFloat = 0.0
-    
-    var onSelect:((Int, String)->Void)?{
-        get{
-            return searchResult?.onSelect
-        }
-        set{
-            searchResult?.onSelect = newValue
-        }
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return self.cellHeight
     }
     
-    required init?(coder aDecoder: NSCoder) {
-        super.init(coder: aDecoder)
-        delegate = self
-        searchResult = DropDownListDataSource()
-        searchResult?.cellIdentifier = "dropDownCell"
-        searchResult?.onSelect = self.onSelect
-        self.addTarget(self, action: #selector(textFieldDidChange(_:)), for: .editingChanged)
-        setupDropdownTable()
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let selectedWord = self.filteredWords[indexPath.row]
+        self.onWordSelected?(selectedWord)
     }
     
-    func setupDropdownTable() {
-        let count = Int(searchResult?.items?.count ?? 0)
-        dropDownTable = UITableView(frame: CGRect(x: 0, y: 0, width: Int((dropDownListPlaceholder?.frame.size.width) ?? 0), height: 44 * count), style: .plain)
-        dropDownTable?.register(UITableViewCell.self, forCellReuseIdentifier: (searchResult?.cellIdentifier)!)
-        dropDownTable?.dataSource = searchResult
-        dropDownTable?.delegate = searchResult
-        dropDownTable?.allowsSelection = true
-        dropDownTable?.isUserInteractionEnabled = true
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: self.cellIdentifier, for: indexPath)
+        cell.textLabel?.text = self.filteredWords[indexPath.row]
+        return cell
     }
     
-    func clean() {
-        searchResult?.items = []
+    func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
+        return self.footerHeight
     }
     
-    func hideDropDown(){
-        dropDownListPlaceholder?.isHidden = true
-        clean()
+    func tableView(_ tableView: UITableView, viewForFooterInSection section: Int) -> UIView? {
+        let footerHintLabel = UILabel(frame: CGRect(x: 1, y: 1, width: tableView.frame.width, height: self.footerHeight))
+        footerHintLabel.text = "Tap to select the word"
+        footerHintLabel.backgroundColor = UIColor.init(hex: "#DDDDDD")
+        return footerHintLabel
     }
     
-    func showDropDown(){
-        dropDownListPlaceholder?.isHidden = false
-    }
-    
-    func updatePlaceholder(position:Int){
-        vertPosition = CGFloat(position)
-        dropDownTable?.frame.size.height = CGFloat((searchResult?.items?.count) ?? 0) * CGFloat(44.0);
-        dropDownListPlaceholder?.frame = CGRect(x: Int((dropDownListPlaceholder?.frame.origin.x)!), y: position, width: 200, height: Int(CGFloat((searchResult?.items?.count) ?? 0) * CGFloat(44.0)))
-    }
-    
-    func textFieldDidEndEditing(_ textField: UITextField, reason: UITextFieldDidEndEditingReason) {
-        dropDownListPlaceholder?.isHidden = true
-        dropDownTable?.removeFromSuperview()
-    }
-    
-    func textFieldDidBeginEditing(_ textField: UITextField) {
-        dropDownListPlaceholder?.isHidden = false
-        dropDownListPlaceholder?.addSubview(dropDownTable!)
-        onTextChanged?()
-    }
-    
-    @objc func textFieldDidChange(_ textField: UITextField) {
-        updateSearchResult(with: textField.text ?? "")
-        dropDownTable?.frame.size.height = CGFloat(((searchResult?.items?.count) ?? 0 >= 4 ? 4 : searchResult?.items?.count ?? 0)) * CGFloat(44.0);
-        dropDownListPlaceholder?.frame.size.height = CGFloat((searchResult?.items?.count) ?? 0) * CGFloat(44.0);
-        if (searchResult?.items?.count)! == 0 {
-            hideDropDown()
-        } else {
-            showDropDown()
-        }
-        dropDownTable?.reloadData()
-        
-    }
-    
-    private func updateSearchResult(with text: String){
-        searchResult?.items = itemsToSearch?.filter({ return ($0.lowercased().hasPrefix(text.lowercased()) && text.count > 2) })
-    }
-    
-    func updateSearchResults(){
-        let text = self.text ?? ""
-        updateSearchResult(with: text)
+    func tableHeight() -> CGFloat {
+        return (CGFloat(self.filteredWords.count) * self.cellHeight) + self.footerHeight
     }
 }
