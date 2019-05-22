4c5bca5ed459dbf37668f247b02d11b58bf3d188
quacklabs <donhoenix@gmail.com>
2019-05-22 17:19:29 +0100

    Fixed PIN/Password Tab accessibility
    - added border to bottom and right or left
    - created function extending projects Button class to add or remove borders accordingly

--- Decred Wallet/Custom Views/Button.swift
+++ Decred Wallet/Custom Views/Button.swift
@@ -30,4 +30,37 @@ class Button: UIButton {
             self.setNeedsLayout()
         }
     }
+    
+    func border(at: String, colorHex: String? = "#333333", thickness: CGFloat? = 1.7){
+        let border = CALayer()
+        border.backgroundColor = UIColor.init(hex: colorHex!).cgColor
+        border.name = at
+        switch at{
+        case "left":
+            border.frame = CGRect(x: 0, y: 0, width: thickness!, height: frame.size.height)
+        case "right":
+            border.frame = CGRect(x: frame.size.width - thickness!, y: 0, width: thickness!, height: frame.size.height)
+        case "bottom":
+            border.frame = CGRect(x: 0, y: frame.size.height - thickness!, width: frame.size.width, height: thickness!)
+        case "top":
+            border.frame = CGRect(x: 0, y: 0, width: frame.size.width, height: thickness!)
+        default:
+            break
+        }
+        layer.addSublayer(border)
+        self.setNeedsLayout()
+    }
+    
+    func removeBorders(){
+        if let layers = layer.sublayers {
+            for currentLayer in layers{
+                if currentLayer.name == "bottom" || currentLayer.name == "left" || currentLayer.name == "right" || currentLayer.name == "top"{
+                    currentLayer.removeFromSuperlayer()
+                }
+            }
+            self.setNeedsLayout()
+        }
+    }
+    
+    
 }
