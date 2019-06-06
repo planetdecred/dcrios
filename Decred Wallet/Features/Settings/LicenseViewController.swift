b6bee1000ccdb605a1ba3cc8322e1e21beccfba6
Suleiman Abubakar <abubakars29@yahoo.com>
2019-06-06 13:28:43 +0100

    License show in settings

---
+++ Decred Wallet/Features/Settings/LicenseViewController.swift
@@ -0,0 +1,20 @@
+//
+//  LicenseViewController.swift
+//  Decred Wallet
+//
+//  Created by Suleiman Abubakar on 06/06/2019.
+//  Copyright (c) 2018-2019 The Decred developers
+//  Use of this source code is governed by an ISC
+//  license that can be found in the LICENSE file.
+//
+
+import UIKit
+
+class LicenseViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        self.navigationItem.title = "License"
+    }
+}
