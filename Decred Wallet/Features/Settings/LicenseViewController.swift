27f0503facd567d284d7258a237fd8c8120c4d0b
Abubakar Suleiman <abubakars29@yahoo.com>
2019-06-06 21:27:22 +0100

    Settings: add button to show open source license (#450)
    * License show in settings
    
    * add disclosure indicator to button, connection link and fix contraint
    
    * fix text wrap and spacing
    
    * fix text color and delete generated viewDidLoD comment
    
    * dark gray color used for license text

---
+++ Decred Wallet/Features/Settings/LicenseViewController.swift
@@ -0,0 +1,19 @@
+//
+//  LicenseViewController.swift
+//  Decred Wallet
+//
+//  Created by Suleiman Abubakar on 06/06/2019.
+//  Copyright (c) 2018-2019 The Decred developers
+//  Use of this source code is governed by an ISC
+//  license that can be found in the LICENSE file.
+//
+
+import UIKit
+
+class LicenseViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.navigationItem.title = "License"
+    }
+}
