c88797a8f9260fa6ee88b2d082c2b2b758c1e294
quacklabs <donhoenix@gmail.com>
2019-09-10 12:02:07 +0100

    created intial files
    - nothing working here yet

---
+++ Decred Wallet/Features/Navigation Menu/NavigationMenu.swift
@@ -0,0 +1,22 @@
+//
+//  NavigationMenu.swift
+//  Decred Wallet
+//
+//  Created by Sprinthub on 10/09/2019.
+//  Copyright © 2019 Decred. All rights reserved.
+//
+
+import UIKit
+import Dcrlibwallet
+
+class NavigationMenu: UITabBarController{
+    
+    convenience init(items: [MenuItem]){
+        self.init()
+        return
+    }
+    
+    public func setupMenu(items: [MenuItem]){
+        
+    }
+}
