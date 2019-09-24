3f9c8294c996122d7a07196de074f14ece8d8c26
Wisdom Arerosuoghene <wisdom.arerosuoghene@gmail.com>
2019-09-24 12:19:04 +0100

    correct copyright header

--- Decred Wallet/DcrlibwalletTypes/Transaction.swift
+++ Decred Wallet/DcrlibwalletTypes/Transaction.swift
@@ -2,9 +2,9 @@
 //  Transaction.swift
 //  Decred Wallet
 //
-//  Created by Wisdom Arerosuoghene on 16/09/2019.
-//  Copyright © 2019 Decred. All rights reserved.
-//
+// Copyright (c) 2018-2019 The Decred developers
+// Use of this source code is governed by an ISC
+// license that can be found in the LICENSE file.
 
 import Foundation
 import Dcrlibwallet
