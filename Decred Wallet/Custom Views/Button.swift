f29bdab3ffe51a791d2d779009ac2e72e3cf1385
bgptr <bgptr@protonmail.com>
2019-12-27 18:46:27 +0100

    fix review comments (custom button)

--- Decred Wallet/Custom Views/Button.swift
+++ Decred Wallet/Custom Views/Button.swift
@@ -10,118 +10,125 @@ import UIKit
 
 @IBDesignable
 class Button: UIButton {
-    private let loaderIcon: UIImageView = UIImageView()
-    private let loaderLabel: UILabel = UILabel()
-    private var loaderView: UIView?
-    private var originalButtonText: String?
-    private var originalButtonBackground: UIColor?
+	private let loaderIcon: UIImageView = UIImageView()
+	private let loaderLabel: UILabel = UILabel()
+	private var originalButtonText: String?
+    private var isLoading: Bool = false
     
-    @IBInspectable var loaderTextStringKey: String = "" {
-        didSet {
-            loaderLabel.text = NSLocalizedString(loaderTextStringKey, comment: "")
-        }
-    }
-
-    @IBInspectable var borderColor: UIColor = UIColor.clear {
-        didSet {
-            setupView()
-        }
-    }
-    
-    @IBInspectable var borderWidth: CGFloat = 0 {
-        didSet {
-            setupView()
-        }
-    }
-    
-    @IBInspectable var cornerRadius: CGFloat = 0 {
-        didSet {
-           setupView()
-        }
-    }
-    
-    @IBInspectable var enabledBackgroundColor : UIColor? {
-        didSet {
-            setupView()
-        }
-    }
-
-    @IBInspectable var disabledBackgroundColor : UIColor? {
-        didSet {
-            setupView()
-        }
-    }
-    
-    override var isEnabled: Bool {
-        didSet {
-            setupView()
-        }
-    }
-    
-    fileprivate func setupView() {
-        if isEnabled && enabledBackgroundColor != nil {
-            self.backgroundColor = enabledBackgroundColor
-        } else if !isEnabled && disabledBackgroundColor != nil {
-            self.backgroundColor = disabledBackgroundColor
-        }
+    lazy private var loaderView: UIView = {
+        let loaderView = UIView()
+        loaderView.isHidden = true
+        self.addSubview(loaderView)
         
-        self.layer.borderColor = self.borderColor.cgColor
-        self.layer.borderWidth = self.borderWidth
-        self.layer.cornerRadius = cornerRadius
-        self.setNeedsDisplay()
-    }
-    
-    public func startLoading() {
-        if loaderView == nil {
-            loaderView = UIView()
-            self.addSubview(loaderView!)
-            loaderView!.isHidden = true
-            loaderView!.translatesAutoresizingMaskIntoConstraints = false
-            loaderView!.centerXAnchor.constraint(equalTo: self.centerXAnchor).isActive = true
-            loaderView!.centerYAnchor.constraint(equalTo: self.centerYAnchor).isActive = true
+        loaderView.translatesAutoresizingMaskIntoConstraints = false
+        loaderView.centerXAnchor.constraint(equalTo: self.centerXAnchor).isActive = true
+        loaderView.centerYAnchor.constraint(equalTo: self.centerYAnchor).isActive = true
 
-            loaderView!.addSubview(loaderIcon)
-            loaderIcon.translatesAutoresizingMaskIntoConstraints = false
-            loaderIcon.image = UIImage(named: "btn_spinner")
-            loaderIcon.widthAnchor.constraint(equalToConstant: 20).isActive = true
-            loaderIcon.heightAnchor.constraint(equalToConstant: 20).isActive = true
+        loaderView.addSubview(loaderIcon)
+        loaderIcon.translatesAutoresizingMaskIntoConstraints = false
+        loaderIcon.image = UIImage(named: "btn_spinner")
+        loaderIcon.widthAnchor.constraint(equalToConstant: 20).isActive = true
+        loaderIcon.heightAnchor.constraint(equalToConstant: 20).isActive = true
+        loaderIcon.leadingAnchor.constraint(equalTo: loaderView.leadingAnchor).isActive = true
+        loaderIcon.topAnchor.constraint(equalTo: loaderView.topAnchor).isActive = true
+        loaderIcon.bottomAnchor.constraint(equalTo: loaderView.bottomAnchor).isActive = true
 
-            loaderIcon.leadingAnchor.constraint(equalTo: loaderView!.leadingAnchor).isActive = true
-            loaderIcon.topAnchor.constraint(equalTo: loaderView!.topAnchor).isActive = true
-            loaderIcon.bottomAnchor.constraint(equalTo: loaderView!.bottomAnchor).isActive = true
-
-            loaderView!.addSubview(loaderLabel)
-            loaderLabel.translatesAutoresizingMaskIntoConstraints = false
-            loaderLabel.font = UIFont(name: "SourceSansPro-Regular", size: 18)
-            loaderLabel.textColor = UIColor.appColors.grayishBlue
-            loaderLabel.leadingAnchor.constraint(equalTo: loaderIcon.trailingAnchor, constant: 10).isActive = true
-            loaderLabel.trailingAnchor.constraint(equalTo: loaderView!.trailingAnchor).isActive = true
-            loaderLabel.centerYAnchor.constraint(equalTo: loaderIcon.centerYAnchor).isActive = true
-        }
+        loaderView.addSubview(loaderLabel)
+        loaderLabel.translatesAutoresizingMaskIntoConstraints = false
+        loaderLabel.font = UIFont(name: "SourceSansPro-Regular", size: 18)
+        loaderLabel.textColor = UIColor.appColors.grayishBlue
+        loaderLabel.leadingAnchor.constraint(equalTo: loaderIcon.trailingAnchor, constant: 10).isActive = true
+        loaderLabel.trailingAnchor.constraint(equalTo: loaderView.trailingAnchor).isActive = true
+        loaderLabel.centerYAnchor.constraint(equalTo: loaderIcon.centerYAnchor).isActive = true
         
-        DispatchQueue.main.async {
-            self.originalButtonBackground = self.backgroundColor
-            self.originalButtonText = self.titleLabel?.text ?? ""
+        return loaderView
+    }()
+    
+	@IBInspectable var loaderTextStringKey: String = "" {
+		didSet {
+			loaderLabel.text = NSLocalizedString(loaderTextStringKey, comment: "")
+		}
+	}
+
+	@IBInspectable var borderColor: UIColor = UIColor.clear {
+		didSet {
+			setupView()
+		}
+	}
+	
+	@IBInspectable var borderWidth: CGFloat = 0 {
+		didSet {
+			setupView()
+		}
+	}
+	
+	@IBInspectable var cornerRadius: CGFloat = 0 {
+		didSet {
+		   setupView()
+		}
+	}
+	
+    @IBInspectable var enabledBackgroundColor: UIColor = UIColor.appColors.decredBlue {
+		didSet {
+            setupView()
+		}
+	}
+
+    @IBInspectable var disabledBackgroundColor : UIColor = UIColor.appColors.lighterGray {
+		didSet {
+            setupView()
+		}
+	}
+	
+	override var isEnabled: Bool {
+		didSet {
+			setupView()
+		}
+	}
+	
+	fileprivate func setupView() {
+		self.layer.borderColor = self.borderColor.cgColor
+		self.layer.borderWidth = self.borderWidth
+		self.layer.cornerRadius = cornerRadius
+        
+        self.updateBackgroundColor()
+	}
+    
+    private func updateBackgroundColor() {
+        if self.isLoading {
             self.backgroundColor = .clear
-            self.setTitle("", for: .normal)
-            self.loaderView!.isHidden = false
-            self.loaderIcon.layer.removeAllAnimations()
-            
-            let rotation : CABasicAnimation = CABasicAnimation(keyPath: "transform.rotation.z")
-            rotation.toValue = NSNumber(value: Double.pi * 2)
-            rotation.duration = 1
-            rotation.isCumulative = true
-            rotation.repeatCount = Float.greatestFiniteMagnitude
-            self.loaderIcon.layer.add(rotation, forKey: "rotationAnimation")
+        } else {
+            self.backgroundColor = !isEnabled ? disabledBackgroundColor : enabledBackgroundColor
         }
     }
-    
-    public func stopLoading() {
-        DispatchQueue.main.async {
-            self.setTitle(self.originalButtonText, for: .normal)
-            self.backgroundColor = self.originalButtonBackground
-            self.loaderIcon.layer.removeAllAnimations()
-            self.loaderView?.isHidden = true
-        }
-    }
+	
+	public func startLoading() {
+		DispatchQueue.main.async {
+            self.isLoading = true
+            self.updateBackgroundColor()
+			self.originalButtonText = self.title(for: .normal)
+			self.setTitle("", for: .normal)
+			self.loaderView.isHidden = false
+			self.loaderIcon.layer.removeAllAnimations()
+			
+			let rotation = CABasicAnimation(keyPath: "transform.rotation.z")
+			rotation.toValue = NSNumber(value: Double.pi * 2)
+			rotation.duration = 1
+			rotation.isCumulative = true
+			rotation.repeatCount = Float.greatestFiniteMagnitude
+			self.loaderIcon.layer.add(rotation, forKey: "rotationAnimation")
+		}
+	}
+	
+	public func stopLoading() {
+		DispatchQueue.main.async {
+            self.updateBackgroundColor()
+			self.setTitle(self.originalButtonText, for: .normal)
+			self.loaderIcon.layer.removeAllAnimations()
+            if self.isLoading {
+                self.loaderView.isHidden = true
+            }
+            self.isLoading = false
+		}
+	}
 }
