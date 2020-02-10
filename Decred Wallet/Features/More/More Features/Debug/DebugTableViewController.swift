d13ea50903491533830ba5ce0474bd7e9e8cd091
macsleven <abubakars29@yahoo.com>
2020-02-10 21:44:54 +0100

    add missing files

---
+++ Decred Wallet/Features/More/More Features/Debug/DebugTableViewController.swift
@@ -0,0 +1,81 @@
+//
+//  DebugTableViewController.swift
+//  Decred Wallet
+
+// Copyright (c) 2019 The Decred developers
+// Use of this source code is governed by an ISC
+// license that can be found in the LICENSE file.
+
+import Foundation
+import UIKit
+import JGProgressHUD
+import Dcrlibwallet
+
+class DebugTableViewController: UITableViewController  {
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        self.navigationController?.navigationBar.isHidden = false
+        self.navigationController?.navigationBar.tintColor = UIColor.appColors.darkBlue 
+        self.navigationController?.navigationBar.barTintColor = UIColor.appColors.offWhite
+        //Remove shadow from navigation bar
+        self.navigationController?.navigationBar.setBackgroundImage(UIImage(), for: UIBarMetrics.default)
+        self.navigationController?.navigationBar.shadowImage = UIImage()
+        
+        self.addNavigationBackButton()
+            
+        let barButtonTitle = UIBarButtonItem(title: LocalizedStrings.debug, style: .plain, target: self, action: nil)
+        barButtonTitle.tintColor = UIColor.black // UIColor.appColor.darkblue
+            
+        self.navigationItem.leftBarButtonItems =  [(self.navigationItem.leftBarButtonItem)!, barButtonTitle]
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if indexPath.row == 1 {
+            // rescan blockchain
+            self.showOkAlert(message: LocalizedStrings.rescanConfirm,
+                             title: LocalizedStrings.rescanBlockchain,
+                             onPressOk: self.rescanBlocks,
+                             addCancelAction: true)
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        let isWalletOpen = AppDelegate.walletLoader.wallet?.walletOpened() ?? false
+        
+        switch indexPath.row {
+        case 1: // rescan blockchain options, requires wallet to be opened.
+            return isWalletOpen ? 44 : 0
+            
+        default:
+            return 44
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 16
+    }
+    
+    func rescanBlocks() {
+        if AppDelegate.walletLoader.wallet!.isSyncing() {
+            self.showOkAlert(message: LocalizedStrings.syncProgressAlert)
+            return
+        }
+        
+        do {
+            try AppDelegate.walletLoader.wallet?.rescanBlocks()
+            self.displayToast(LocalizedStrings.scanInProgress)
+        } catch let error {
+            var errorMessage = error.localizedDescription
+            if errorMessage == DcrlibwalletErrInvalid {
+                errorMessage = LocalizedStrings.scanStartedAlready
+            }
+            self.showOkAlert(message: errorMessage, title: LocalizedStrings.rescanFailed)
+        }
+    }
+    
+    static func instantiate() -> Self {
+        return Storyboards.Settings.instantiateViewController(for: self)
+    }
+}
---
+++ Decred Wallet/Features/More/More Features/Debug/WalletLogViewController.swift
@@ -0,0 +1,51 @@
+//
+//  WalletLogViewController.swift
+//  Decred Wallet
+//
+// Copyright (c) 2018-2019 The Decred developers
+// Use of this source code is governed by an ISC
+// license that can be found in the LICENSE file.
+
+import UIKit
+import JGProgressHUD
+
+class WalletLogViewController: UIViewController {
+    @IBOutlet weak var logTextView: UITextView!
+    var progressHud = JGProgressHUD(style: .light)
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.navigationItem.title = LocalizedStrings.walletLog
+        self.progressHud = Utils.showProgressHud(withText: LocalizedStrings.loading)
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(title: LocalizedStrings.copy, style: .plain, target: self, action: #selector(copyLog))
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        self.logTextView.text = readLog()
+    }
+    
+    @objc func copyLog() -> Void {
+        DispatchQueue.main.async {
+            UIPasteboard.general.string = self.logTextView.text
+            self.showOkAlert(message: LocalizedStrings.walletLogCopied)
+        }
+    }
+    
+    private func readLog() -> String {
+        do {
+            let logPath = "\(WalletLoader.appDataDir)/\(BuildConfig.NetType)/dcrlibwallet.log"
+            let logContent = try String(contentsOf: URL(fileURLWithPath: logPath))
+            let logEntries = logContent.split(separator: "\n")
+            if logEntries.count > 500 {
+                self.progressHud.dismiss()
+                return logEntries.suffix(from: logEntries.count - 500).joined(separator: ";\n")
+            } else {
+                self.progressHud.dismiss()
+                return logEntries.suffix(from: 0).joined(separator: ";\n")
+            }
+        } catch (let error) {
+            self.progressHud.dismiss()
+            return "Error loading log: \(error.localizedDescription)"
+        }
+    }
+}
