818e4e598cf25c4cb74ad6cc5f5e42a4bb9849dd
macsleven <abubakars29@yahoo.com>
2020-04-29 17:28:52 +0100

    update stats details real time

--- Decred Wallet/Features/More/More Features/Debug/StatisticsViewController.swift
+++ Decred Wallet/Features/More/More Features/Debug/StatisticsViewController.swift
@@ -26,6 +26,8 @@ class StatisticsViewController: UITableViewController  {
     @IBOutlet weak var transactionDetailLabel: UILabel!
     @IBOutlet weak var accountDetailLabel: UILabel!
     
+    var refreshStatsTimer: Timer?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.tableView.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: 36, right: 0)
@@ -37,27 +39,51 @@ class StatisticsViewController: UITableViewController  {
         self.setupNavigationBar()
     }
     
+    override func viewWillDisappear(_ animated: Bool) {
+        // stop refreshing statistics when view becomes invisible
+        self.refreshStatsTimer?.invalidate()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        self.refreshStatsTimer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true) {_ in
+            DispatchQueue.main.async {
+                self.statsLiveUpdate()
+            }
+        }
+    }
+    
     func loadStatistcs() {
         let dateformater = DateFormatter()
         dateformater.dateFormat = "yyyy-MM-dd"
+        // build date
         self.buildDetailLabel.text = "\(BuildConfig.NetType), \(dateformater.string(from: AppDelegate.compileDate as Date))"
+        self.networkDetailLabel.text = "\(BuildConfig.NetType)"
+        // wallet file directory
+        self.walletFileDetailLabel.text = "/Documents/dcrlibwallet/\(BuildConfig.NetType)/"
+        // opened wallets account
+        self.accountDetailLabel.text = "\(WalletLoader.shared.multiWallet.openedWalletsCount())"
+        // load live update details
+        self.statsLiveUpdate()
+    }
+    
+    func statsLiveUpdate(){
+        // numbers of peers
         self.peerConnectedDetailLabel.text = "\(WalletLoader.shared.multiWallet.connectedPeers())"
-        
+        // app uptime
         let durationUpTime = Date().timeIntervalSince1970 - AppDelegate.appUpTime!
         self.uptimeDetailLabel.text = "\(Utils.timeStringFor(seconds: durationUpTime))"
-        
-        self.networkDetailLabel.text = "\(BuildConfig.NetType)"
+        // best block
         self.bestBlockDetailLabel.text = "\(WalletLoader.shared.multiWallet.getBestBlock()?.height ?? 0)"
-        
+        // best block age info
         let bestBlockInfo = WalletLoader.shared.multiWallet.getBestBlock()
-        let bestBlockAge = Int64(Date().timeIntervalSince1970) - bestBlockInfo!.timestamp
         self.bestBlockTimestampDetailLabel.text = "\(Date(timeIntervalSince1970: Double(bestBlockInfo!.timestamp)))"
+        // best block age
+        let bestBlockAge = Int64(Date().timeIntervalSince1970) - bestBlockInfo!.timestamp
         self.bestBlockAgeDetailLabel.text = Utils.calculateTime(timeInterval: bestBlockAge).lowercased()
-        
-        self.walletFileDetailLabel.text = "/Documents/dcrlibwallet/\(BuildConfig.NetType)/"
+        // Transaction count
+         self.transactionDetailLabel.text = "\(Utils.countAllWalletTransaction())"
+        // chain Data in human readable format
         self.chainDataDetailLabel.text = "\(Utils.format(bytes: Double(Utils.getDirFileSize())))"
-        self.accountDetailLabel.text = "\(WalletLoader.shared.multiWallet.openedWalletsCount())"
-        self.transactionDetailLabel.text = "\(Utils.countAllWalletTransaction())"
     }
     
     private func setupNavigationBar() {
