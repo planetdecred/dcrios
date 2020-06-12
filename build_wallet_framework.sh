a8b4fc18630398709d2c9f954432cd4756920e34
Collins Olanrewaju <ocollins444@gmail.com>
2020-06-12 13:34:20 +0100

    Update dcrlibwallet build script
    This adds functions to install golang & gomobile if missing and removes
    v1.0.0 checkout from the build process.

--- build_wallet_framework.sh
+++ build_wallet_framework.sh
@@ -1,57 +1,68 @@
 #!/bin/bash
 export LIB_DIR=$(pwd)/libs
 export DCRLIBWALLET_GIT_DIR=$GOPATH/src/github.com/raedahgroup/dcrlibwallet
+export DCRLIBWALLET_REPO_URL=https://github.com/raedahgroup/dcrlibwallet.git
 
-main() {
-	if [ -e $DCRLIBWALLET_GIT_DIR ]
-	then
-		echo "dcrlibwallet git folder found, running git pull"
-		updateDcrlibwallet;
-	else
-		echo "dcrlibwallet git folder does not exist, running git clone"
-		cloneDcrlibwallet;
-	fi
+installGo(){
+    echo "Installing golang"
+    curl -O https://storage.googleapis.com/golang/go1.12.9.darwin-amd64.tar.gz
+    sha256sum go1.12.9.darwin-amd64.tar.gz
+    tar -xvf go1.12.9.darwin-amd64.tar.gz
+    sudo chown -R root:root ./go
+    sudo mv go /usr/local
+	export GOPATH=$HOME/go
+	export PATH=$PATH:/usr/local/go/bin:$GOPATH/bin
+}
 
-	echo "building dcrlibwallet"
-	buildDcrlibwallet;
-
-	echo "copying built binary"
-	copyLibrary;
+installGomobile(){
+    echo "Installing gomobile"
+    go get -u golang.org/x/mobile/cmd/gomobile
+    gomobile init
 }
 
 cloneDcrlibwallet() {
 	rm -rf $DCRLIBWALLET_GIT_DIR
 	mkdir -p $DCRLIBWALLET_GIT_DIR
-	git clone https://github.com/raedahgroup/dcrlibwallet.git $DCRLIBWALLET_GIT_DIR
-	# update with the appropriate tag/commit hash to checkout 
-	git checkout v1.0.0
-	echo "done cloning dcrlibwallet"
+	git clone $DCRLIBWALLET_GIT_DIR
+	echo "Dcrlibwallet clone complete"
 }
 
 updateDcrlibwallet() {
 	cd $DCRLIBWALLET_GIT_DIR
-	git fetch
-	# update with the appropriate tag/commit hash to checkout 
-	git checkout v1.0.0
-	echo "done updating dcrlibwallet"
+	git pull $DCRLIBWALLET_REPO_URL master
+	echo "Dcrlibwallet update complete"
 }
 
 buildDcrlibwallet() {
+
+	if !(hash go 2>/dev/null); then
+		installGo
+	fi
+
+	if !(hash gomobile 2>/dev/null); then
+		installGomobile
+	fi
+
 	cd $DCRLIBWALLET_GIT_DIR
 	rm -rf Dcrlibwallet.framework/
-	export GO111MODULE=on
-	go mod download
-	go mod vendor
-	export GO111MODULE=off
+	export GO111MODULE=on && go mod vendor && export GO111MODULE=off
 	gomobile bind -target=ios
-	echo "done building dcrlibwallet"
 }
 
-copyLibrary() {
-	rm -rf $LIB_DIR
-	mkdir $LIB_DIR
-	cp -R -f Dcrlibwallet.framework $LIB_DIR/Dcrlibwallet.framework
-	echo "done"
-}
+if [ -e $DCRLIBWALLET_GIT_DIR ]
+then
+	echo "Dcrlibwallet folder exists in GOPATH, updating from master"
+	updateDcrlibwallet;
+else
+	echo "Dcrlibwallet folder does not exist in GOPATH, cloning from master"
+	cloneDcrlibwallet;
+fi
 
-main
+echo "Building Dcrlibwallet Framework"
+buildDcrlibwallet;
+
+echo "Coping Dcrlibwallet framework to $LIB_DIR"
+mkdir $LIB_DIR
+rm -rf $LIB_DIR/Dcrlibwallet.Framework
+cp -R -f Dcrlibwallet.framework $LIB_DIR/Dcrlibwallet.framework
+echo "Done"
