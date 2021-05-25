bff3583ae33ea82c0b479a017fba19f2079ab74e
dreacot <kennedyizuegbu@yahoo.com>
2021-05-25 22:15:52 +0100

    verify current password immediately after inputting it

--- Decred Wallet/Wallet Utils/SpendingPinOrPassword.swift
+++ Decred Wallet/Wallet Utils/SpendingPinOrPassword.swift
@@ -14,22 +14,44 @@ struct SpendingPinOrPassword {
         Security.spending(initialSecurityType: SpendingPinOrPassword.securityType(for: walletID))
             .with(prompt: LocalizedStrings.confirmToChange)
             .with(submitBtnText: LocalizedStrings.confirm)
-            .requestCurrentAndNewCode(sender: sender) {
-                currentCode, currentCodeRequestDelegate, newCode, newCodeRequestDelegate, newCodeType in
+            .requestCurrentCode(sender: sender) {
+                currentCode, securityType, currentCodeRequestDelegate in
                 
-                self.changeWalletSpendingPassphrase(walletID: walletID,
+                self.verifyWalletSpendingPassphrase(walletID: walletID,
                                                     currentCode: currentCode,
                                                     currentCodeRequestDelegate: currentCodeRequestDelegate,
-                                                    newCode: newCode,
-                                                    newCodeRequestDelegate: newCodeRequestDelegate,
-                                                    newCodeType: newCodeType,
+                                                    securityType: securityType,
                                                     done: done)
+        }
+    }
+    
+    private static func verifyWalletSpendingPassphrase(walletID: Int,
+                                                       currentCode: String,
+                                                       currentCodeRequestDelegate: InputDialogDelegate?,
+                                                       securityType: SecurityType,
+                                                       done: (() -> ())?) {
+        
+        DispatchQueue.global(qos: .userInitiated).async {
+            do {
+                let wallet = WalletLoader.shared.multiWallet.wallet(withID: walletID)
+                try wallet?.unlock(currentCode.utf8Bits)
+                wallet?.lock()
+                DispatchQueue.main.async {
+                    currentCodeRequestDelegate?.dismissDialog()
+                    done?()
+                }
+            } catch let error {
+                DispatchQueue.main.async {
+                    if error.isInvalidPassphraseError {
+                        currentCodeRequestDelegate?.displayPassphraseError(errorMessage: self.invalidSecurityCodeMessage(for: walletID))
+                    }
+                }
+            }
         }
     }
     
     private static func changeWalletSpendingPassphrase(walletID: Int,
                                                        currentCode: String,
-                                                       currentCodeRequestDelegate: InputDialogDelegate?,
                                                        newCode: String,
                                                        newCodeRequestDelegate: InputDialogDelegate?,
                                                        newCodeType: SecurityType,
@@ -44,14 +66,13 @@ struct SpendingPinOrPassword {
                 
                 DispatchQueue.main.async {
                     newCodeRequestDelegate?.dismissDialog()
-                    currentCodeRequestDelegate?.dismissDialog()
                     done?()
                 }
             } catch let error {
                 DispatchQueue.main.async {
                     if error.isInvalidPassphraseError {
                         newCodeRequestDelegate?.dismissDialog()
-                        currentCodeRequestDelegate?.displayPassphraseError(errorMessage: self.invalidSecurityCodeMessage(for: walletID))
+//                        currentCodeRequestDelegate?.displayPassphraseError(errorMessage: self.invalidSecurityCodeMessage(for: walletID))
                     } else {
                         newCodeRequestDelegate?.displayError(errorMessage: error.localizedDescription)
                     }
@@ -59,6 +80,39 @@ struct SpendingPinOrPassword {
             }
         }
     }
+    
+//    private static func changeWalletSpendingPassphrase(walletID: Int,
+//                                                       currentCode: String,
+//                                                       currentCodeRequestDelegate: InputDialogDelegate?,
+//                                                       newCode: String,
+//                                                       newCodeRequestDelegate: InputDialogDelegate?,
+//                                                       newCodeType: SecurityType,
+//                                                       done: (() -> ())?) {
+//
+//        DispatchQueue.global(qos: .userInitiated).async {
+//            do {
+//                try WalletLoader.shared.multiWallet.changePrivatePassphrase(forWallet: walletID,
+//                                                                            oldPrivatePassphrase: currentCode.utf8Bits,
+//                                                                            newPrivatePassphrase: newCode.utf8Bits,
+//                                                                            privatePassphraseType: newCodeType.type)
+//
+//                DispatchQueue.main.async {
+//                    newCodeRequestDelegate?.dismissDialog()
+//                    currentCodeRequestDelegate?.dismissDialog()
+//                    done?()
+//                }
+//            } catch let error {
+//                DispatchQueue.main.async {
+//                    if error.isInvalidPassphraseError {
+//                        newCodeRequestDelegate?.dismissDialog()
+//                        currentCodeRequestDelegate?.displayPassphraseError(errorMessage: self.invalidSecurityCodeMessage(for: walletID))
+//                    } else {
+//                        newCodeRequestDelegate?.displayError(errorMessage: error.localizedDescription)
+//                    }
+//                }
+//            }
+//        }
+//    }
     
     static func securityType(for walletID: Int) -> SecurityType {
         if let wallet = WalletLoader.shared.multiWallet.wallet(withID: walletID) {
