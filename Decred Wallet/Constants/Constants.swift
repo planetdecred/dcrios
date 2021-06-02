df2a8cfa38a85f0530cc91d19fdb35d463e4e21b
Justin Do <justin.namdo@gmail.com>
2021-06-03 05:16:00 +0700

    Check notification when open app in tap notification (#803)
    * check notification when open app in tap notification
    
    * Add proposal id string constant
    
    Co-authored-by: Collins Olanrewaju <ocollins444@gmail.com>

--- Decred Wallet/Constants/Constants.swift
+++ Decred Wallet/Constants/Constants.swift
@@ -24,6 +24,7 @@ struct GlobalConstants {
         static let MIXED = "mixed"
         static let UNMIXED = "unmixed"
         static let SHOWN_PRIVACY_TOOLTIP = "shown_privacy_tooltip"
+        static let PROPOSAL_ID = "proposalId"
         static let HAS_SETUP_PRIVACY = "has_setup_privacy"
     }
     
