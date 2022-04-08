b974abf43c861e136e8d5417ec433f07b411a496
dreacot <kennedyizuegbu@yahoo.com>
2022-04-08 19:59:26 +0100

    fix app name in script

--- .github/scripts/test_app.sh
+++ .github/scripts/test_app.sh
@@ -3,6 +3,6 @@
 set -eo pipefail
 
 xcodebuild -workspace Decred\ Wallet.xcworkspace \
-            -scheme Decred\ Wallet\ Testnet \ iOS \
+            -scheme Decred\ Wallet\ Testnet \
             -destination platform=iOS\ Simulator,OS=13.3,name=iPhone\ 11 \
             clean test | xcpretty
