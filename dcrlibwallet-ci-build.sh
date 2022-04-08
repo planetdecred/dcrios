c7a90cd68cb57d0cbaa1a2130976e51385d22e4e
dreacot <kennedyizuegbu@yahoo.com>
2022-04-08 20:55:28 +0100

    install gobind

--- dcrlibwallet-ci-build.sh
+++ dcrlibwallet-ci-build.sh
@@ -1,14 +1,21 @@
 installGo(){
     echo "Installing golang"
-    curl -O https://storage.googleapis.com/golang/go1.12.9.darwin-amd64.tar.gz
-    sha256sum go1.12.9.darwin-amd64.tar.gz
-    tar -xvf go1.12.9.darwin-amd64.tar.gz
+    curl -O https://storage.googleapis.com/golang/go1.16.7.darwin-amd64.tar.gz
+    sha256sum go1.16.7.darwin-amd64.tar.gz
+    tar -xvf go1.16.7.darwin-amd64.tar.gz
     sudo chown -R root:root ./go
     sudo mv go /usr/local
 }
 
+installGobind(){
+    echo "Installing gobind"
+    export GO111MODULE=off
+    go get -u golang.org/x/mobile/cmd/gobind
+}
+
 installGomobile(){
     echo "Installing gomobile"
+    export GO111MODULE=off
     go get -u golang.org/x/mobile/cmd/gomobile
     gomobile init
 }
@@ -20,9 +27,14 @@ fi
 export GOPATH=$HOME/go
 export PATH=$PATH:/usr/local/go/bin:$GOPATH/bin
 
+if !(hash gobind 2>/dev/null); then
+    installGobind
+fi
+
 if !(hash gomobile 2>/dev/null); then
     installGomobile
 fi
+
 
 go version
 echo "Building dcrlibwallet"
